b41c73758cc181f4083479f3f5ea13db7062dd74
Le Vivilet <levivlet@pm.me>
2024-08-27 19:31:35 +0200

    remove unused code

--- src/parts/GetPtyHostPath/GetPtyHostPath.js
+++
@@ -1,6 +0,0 @@
-import * as Path from '../Path/Path.js'
-import * as Root from '../Root/Root.js'
-
-export const getPtyHostPath = () => {
-  return Path.join(Root.root, '@lvce-editor', 'pty-host')
-}
