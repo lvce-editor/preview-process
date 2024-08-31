0c2a56deddea2c045369a1a6d7e633194c69aacc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-31 22:31:42 +0200

    feature: handle electron file response (#23)

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -2,11 +2,12 @@ import * as CreateWebViewServer from '../CreateWebViewServer/CreateWebViewServer
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
 import * as SetWebViewServerHandler from '../SetWebViewServerHandler/SetWebViewServerHandler.ts'
 import * as StartWebViewServer from '../StartWebViewServer/StartWebViewServer.ts'
+import * as WebViewProtocol from '../WebViewProtocol/WebViewProtocol.ts'
 
 export const commandMap = {
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
+  'WebViewProtocol.getResponse': WebViewProtocol.getResponse,
   'WebViewServer.create': CreateWebViewServer.createWebViewServer,
   'WebViewServer.setHandler': SetWebViewServerHandler.setWebViewServerHandler,
   'WebViewServer.start': StartWebViewServer.startWebViewServer,
-  'HandleElectronMessagePort.handleElectronMessagePort':
-    HandleElectronMessagePort.handleElectronMessagePort,
 }
