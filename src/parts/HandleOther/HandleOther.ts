238e8cc5ea2587642b942bc5601dfb497ccc59c6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-07 20:46:21 +0200

    feature: improve error handling for not found error (#33)

--- src/parts/HandleOther/HandleOther.ts
+++ src/parts/HandleOther/HandleOther.ts
@@ -1,20 +1,32 @@
-import { readFile } from 'node:fs/promises'
+import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetContentType from '../GetContentType/GetContentType.ts'
+import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.ts'
+import * as IsEnoentError from '../IsEnoentError/IsEnoentError.ts'
 
 export const handleOther = async (filePath: string) => {
   try {
     const contentType = GetContentType.getContentType(filePath)
     // TODO figure out which of these headers are actually needed
-    const content = await readFile(filePath)
+    const content = await FileSystem.readFile(filePath)
     return new Response(content, {
       headers: {
-        'Cross-Origin-Resource-Policy': 'cross-origin',
-        'Cross-Origin-Embedder-Policy': 'require-corp',
-        'Access-Control-Allow-Origin': '*',
+        'Cross-Origin-Resource-Policy': 'cross-origin', // TODO use same origin
+        'Cross-Origin-Embedder-Policy': 'require-corp', // TODO only for index html?
+        'Access-Control-Allow-Origin': '*', // TODO
         'Content-Type': contentType,
       },
     })
   } catch (error) {
+    if (IsEnoentError.isEnoentError(error)) {
+      return new Response('not found', {
+        status: HttpStatusCode.NotFound,
+        headers: {
+          'Cross-Origin-Resource-Policy': 'cross-origin',
+          'Cross-Origin-Embedder-Policy': 'require-corp',
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+    }
     console.error(error)
     return new Response(`[preview-server] ${error}`)
   }
