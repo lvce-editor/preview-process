49ac8ce65ed3fa5769edc8fb78c8c06dddcc3f13
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-07 21:31:57 +0200

    fix: sending response status code (#34)

--- src/parts/CreateWebViewServerHandler/CreateWebViewServerHandler.ts
+++ src/parts/CreateWebViewServerHandler/CreateWebViewServerHandler.ts
@@ -1,7 +1,7 @@
 import type { IncomingMessage, ServerResponse } from 'node:http'
-import { pipeline } from 'node:stream/promises'
 import * as GetPathName from '../GetPathName/GetPathName.ts'
 import * as GetResponse from '../GetResponse/GetResponse.ts'
+import * as SendResponse from '../SendResponse/SendResponse.ts'
 
 export const createHandler = (frameAncestors: string, webViewRoot: string) => {
   const handleRequest = async (request: IncomingMessage, response: ServerResponse): Promise<void> => {
@@ -10,15 +10,7 @@ export const createHandler = (frameAncestors: string, webViewRoot: string) => {
       pathName += 'index.html'
     }
     const result = await GetResponse.getResponse(pathName, frameAncestors, webViewRoot)
-
-    if (!result?.body) {
-      response.end('not found')
-      return
-    }
-    result.headers.forEach((value, key) => {
-      response.setHeader(key, value)
-    })
-    await pipeline(result.body, response)
+    await SendResponse.sendResponse(response, result)
   }
 
   return handleRequest
