8dece4e51c5b02fce5fa361b079ce0d6c81e1073
Le Vivilet <levivlet@pm.me>
2024-09-07 21:30:45 +0200

    fix: sending response status code

---
+++ src/parts/SendResponse/SendResponse.ts
@@ -0,0 +1,14 @@
+import type { ServerResponse } from 'node:http'
+import { pipeline } from 'node:stream/promises'
+
+export const sendResponse = async (response: ServerResponse, result: Response) => {
+  if (!result?.body) {
+    response.end('not found')
+    return
+  }
+  response.statusCode = result.status
+  result.headers.forEach((value, key) => {
+    response.setHeader(key, value)
+  })
+  await pipeline(result.body, response)
+}
