63de4ef149a00c2f249926c50a5882e24df340ed
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-22 09:19:57 +0100

    feature: use explicit return types (#89)

--- src/parts/IpcChild/IpcChild.ts
+++ src/parts/IpcChild/IpcChild.ts
@@ -1,6 +1,7 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 
-export const listen = async ({ method, ...params }: { method: number; [key: string]: any }) => {
+export const listen = async ({ method, ...params }: { method: number; [key: string]: any }): Promise<Rpc> => {
   const module = IpcChildModule.getModule(method)
   // @ts-ignore
   const rpc = await module.create(params)
