34fd64ac44a30c985f864efb93e327953244eb6a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-22 10:09:48 +0100

    feature: enable eslint node plugin (#98)
    * feature: enable eslint node plugin
    
    * fix
    
    * move plugins up
    
    * simplify rules
    
    * fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import perfectionist from 'eslint-plugin-perfectionist'
 import pluginJest from 'eslint-plugin-jest'
+import nodePlugin from 'eslint-plugin-n'
+import perfectionist from 'eslint-plugin-perfectionist'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -12,36 +13,6 @@ export default tseslint.config(
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
-    },
-  },
-  {
-    ignores: [
-      'dist',
-      'coverage',
-      'scripts',
-      'rollup.config.js',
-      'eslint.config.js',
-      'src/previewProcessMain.ts',
-      'src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts',
-      'files/previewInjectedCode.js',
-    ],
-  },
-  {
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/require-await': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/no-deprecated': 'off',
-      '@typescript-eslint/no-unnecessary-condition': 'off',
-      'jest/no-disabled-tests': 'off',
     },
   },
   {
@@ -59,5 +30,46 @@ export default tseslint.config(
       ],
     },
   },
+  nodePlugin.configs['flat/recommended'],
   pluginJest.configs['flat/recommended'],
+  {
+    ignores: [
+      'dist',
+      'coverage',
+      'scripts',
+      'rollup.config.js',
+      'eslint.config.js',
+      'src/previewProcessMain.ts',
+      'src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts',
+      'files/previewInjectedCode.js',
+    ],
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      'n/no-unpublished-import': [
+        'error',
+        {
+          allowModules: ['@jest/globals'],
+        },
+      ],
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/no-deprecated': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      'jest/no-disabled-tests': 'off',
+    },
+  },
 )
