4f5f93ac22ef996206c9cfb370371ffcc03c9651
Le Vivilet <levivlet@pm.me>
2024-12-22 09:55:22 +0100

    lint

--- src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
+++ src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
@@ -1,7 +1,7 @@
 import { WebViewServer } from '../WebViewServerTypes/WebViewServerTypes.ts'
 
 export const waitForServerToBeReady = async (server: WebViewServer, port: string): Promise<void> => {
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type,  n/no-unsupported-features/es-syntax
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type n/no-unsupported-features/es-syntax
   const { resolve, promise } = Promise.withResolvers<void>()
   server.listen(port, resolve)
   await promise
