de2725518d802a9fc28a61ff8ff9648a97a1141c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-22 08:43:55 +0100

    feature: use native promise.withResolvers (#73)

--- src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
+++ src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
@@ -1,8 +1,7 @@
-import * as Promises from '../Promises/Promises.ts'
 import { WebViewServer } from '../WebViewServerTypes/WebViewServerTypes.ts'
 
 export const waitForServerToBeReady = async (server: WebViewServer, port: string) => {
-  const { resolve, promise } = Promises.withResolvers<void>()
+  const { resolve, promise } = Promise.withResolvers<void>()
   server.listen(port, resolve)
   await promise
 }
