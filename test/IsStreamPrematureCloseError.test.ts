beb140c24be9d0f3cc5eac8d9ea56167d2471255
Le Vivilet <levivlet@pm.me>
2024-12-22 08:48:59 +0100

    feature: add stream premature close error test

---
+++ test/IsStreamPrematureCloseError.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@jest/globals'
+import * as IsStreamPrematureCloseError from '../src/parts/IsStreamPrematureCloseError/IsStreamPrematureCloseError.ts'
+import * as ErrorCodes from '../src/parts/ErrorCodes/ErrorCodes.ts'
+
+test('isStreamPrematureCloseError - when error is undefined', () => {
+  expect(IsStreamPrematureCloseError.isStreamPrematureCloseError(undefined)).toBe(false)
+})
+
+test('isStreamPrematureCloseError - when error is null', () => {
+  expect(IsStreamPrematureCloseError.isStreamPrematureCloseError(null)).toBe(false)
+})
+
+test('isStreamPrematureCloseError - when error has no code', () => {
+  expect(IsStreamPrematureCloseError.isStreamPrematureCloseError({})).toBe(false)
+})
+
+test('isStreamPrematureCloseError - when error has different code', () => {
+  expect(IsStreamPrematureCloseError.isStreamPrematureCloseError({ code: 'DIFFERENT_ERROR' })).toBe(false)
+})
+
+test('isStreamPrematureCloseError - when error is stream premature close error', () => {
+  expect(
+    IsStreamPrematureCloseError.isStreamPrematureCloseError({
+      code: ErrorCodes.ERR_STREAM_PREMATURE_CLOSE,
+    }),
+  ).toBe(true)
+})
