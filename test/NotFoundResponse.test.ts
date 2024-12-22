52c19296c501ed777bf8e31a342767c8ccfacd33
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-22 09:12:25 +0100

    feature: sort imports (#85)

--- test/NotFoundResponse.test.ts
+++ test/NotFoundResponse.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
-import * as NotFoundResponse from '../src/parts/NotFoundResponse/NotFoundResponse.ts'
-import * as HttpStatusCode from '../src/parts/HttpStatusCode/HttpStatusCode.ts'
 import * as DefaultHeaders from '../src/parts/DefaultHeaders/DefaultHeaders.ts'
+import * as HttpStatusCode from '../src/parts/HttpStatusCode/HttpStatusCode.ts'
+import * as NotFoundResponse from '../src/parts/NotFoundResponse/NotFoundResponse.ts'
 
 test('create - returns correct not found response', () => {
   expect(NotFoundResponse.create()).toEqual({
