cec32d5e29df17040290ae424b07f2b620fd98d4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-22 09:59:37 +0100

    feature: ignore one eslint rule (#97)
    * feature: ignore one eslint rule
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * lint

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default tseslint.config(
       'rollup.config.js',
       'eslint.config.js',
       'src/previewProcessMain.ts',
+      'src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts',
       'files/previewInjectedCode.js',
     ],
   },
--- src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
+++ src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
@@ -1,7 +1,6 @@
 import { WebViewServer } from '../WebViewServerTypes/WebViewServerTypes.ts'
 
 export const waitForServerToBeReady = async (server: WebViewServer, port: string): Promise<void> => {
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
   const { resolve, promise } = Promise.withResolvers<void>()
   server.listen(port, resolve)
   await promise
