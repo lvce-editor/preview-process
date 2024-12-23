5304ce35c66f40dc75db0cefc428307c02291ea3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-23 23:11:15 +0100

    feature: set cross origin resource policy and cross origin embedder policy (#113)

--- src/parts/GetIndexResponse/GetIndexResponse.ts
+++ src/parts/GetIndexResponse/GetIndexResponse.ts
@@ -1,16 +1,23 @@
 import type { Info } from '../Info.ts'
+import * as CrossOriginEmbedderPolicy from '../CrossOriginEmbedderPolicy/CrossOriginEmbedderPolicy.ts'
+import * as CrossOriginOpenerPolicy from '../CrossOriginOpenerPolicy/CrossOriginOpenerPolicy.ts'
+import * as CrossOriginResourcePolicy from '../CrossOriginResourcePolicy/CrossOriginResourcePolicy.ts'
 import * as GetHeaders from '../GetHeaders/GetHeaders.ts'
 import * as HttpHeader from '../HttpHeader/HttpHeader.ts'
+import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.ts'
 
 export const getIndexResponse = async (info: Info): Promise<any> => {
   const headers = GetHeaders.getHeaders('/test/index.html')
   return {
     body: info.iframeContent,
     init: {
-      status: 200,
+      status: HttpStatusCode.Ok,
       headers: {
         ...headers,
         [HttpHeader.ContentSecurityPolicy]: info.contentSecurityPolicy,
+        [HttpHeader.CrossOriginEmbedderPolicy]: CrossOriginEmbedderPolicy.value,
+        [HttpHeader.CrossOriginOpenerPolicy]: CrossOriginOpenerPolicy.value,
+        [HttpHeader.CrossOriginResourcePolicy]: CrossOriginResourcePolicy.value,
       },
     },
   }
