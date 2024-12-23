e96bccfb70b167d9954d8b6a283f75d0593d109b
Le Vivilet <levivlet@pm.me>
2024-12-23 14:38:38 +0100

    feature: support remote urls when running in electron

--- src/parts/WebViewProtocol/WebViewProtocol.ts
+++ src/parts/WebViewProtocol/WebViewProtocol.ts
@@ -4,6 +4,7 @@ import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetHeaders from '../GetHeaders/GetHeaders.ts'
 import * as GetInfo from '../GetInfo/GetInfo.ts'
 import * as GetPathName from '../GetPathName/GetPathName.ts'
+import * as GetRemoteResponse from '../GetRemoteResponse/GetRemoteResponse.ts'
 import * as HttpHeader from '../HttpHeader/HttpHeader.ts'
 import * as HttpMethod from '../HttpMethod/HttpMethod.ts'
 import * as InternalServerErrorResponse from '../InternalServerErrorResponse/InternalServerErrorResponse.ts'
@@ -32,10 +33,14 @@ export const getResponse = async (method: string, url: string): Promise<any> => 
       },
     }
   }
+  // TODO use pathname
   if (url.endsWith('preview-injected.js')) {
     const { injectedCode } = PreviewInjectedCode
     const headers = GetHeaders.getHeaders('/test/file.js')
     return SuccessResponse.create(injectedCode, headers)
+  }
+  if (pathName.startsWith('/remote')) {
+    return GetRemoteResponse.getRemoteResponse(pathName, info.webViewRoot)
   }
   // TODO use stat and etag
   const absolutePath = join(info.webViewRoot, pathName)
