b9deca029297a09a681154f71c5e5791c4f1194d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-23 13:29:53 +0100

    feature: also include content security policy and iframe content in webview handler (#106)
    * feature: also include content security policy and iframe content in webview handler
    
    * fix
    
    * coverage
    
    * fix code
    
    * fix
    
    * test

--- src/parts/SetInfo/SetInfo.ts
+++ src/parts/SetInfo/SetInfo.ts
@@ -1,10 +1,16 @@
-import * as CreateWebViewServerHandler from '../CreateWebViewServerHandler/CreateWebViewServerHandler.ts'
 import * as InfoRegistry from '../InfoRegistry/InfoRegistry.ts'
-import * as WebViewServerState from '../WebViewServerState/WebViewServerState.ts'
 
-export const setInfo = (id: number, webViewId: string, webViewRoot: string): void => {
+export const setInfo = (
+  id: number,
+  webViewId: string,
+  webViewRoot: string,
+  contentSecurityPolicy: string,
+  iframeContent: string,
+): void => {
   InfoRegistry.set(webViewId, {
     webViewId,
     webViewRoot,
+    contentSecurityPolicy,
+    iframeContent,
   })
 }
