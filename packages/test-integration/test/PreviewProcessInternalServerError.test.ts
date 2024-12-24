ad7ccceff0bbac3135684dca4859bc5f55166da8
Le Vivilet <levivlet@pm.me>
2024-12-24 23:50:39 +0100

    feature: try to mock fs in test

--- packages/test-integration/test/PreviewProcessInternalServerError.test.ts
+++ packages/test-integration/test/PreviewProcessInternalServerError.test.ts
@@ -1,28 +1,44 @@
 import { expect, test } from '@jest/globals'
 import getPort from 'get-port'
-import { fileURLToPath } from 'url'
 import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
 import { get } from '../src/parts/Get/Get.js'
 import { getRoot } from '../src/parts/GetRoot/GetRoot.js'
+import CDP from 'chrome-remote-interface'
 
 test('preview process - internal server error', async () => {
   const debugPort = await getPort()
   const previewProcess = createPreviewProcess({
-    execArgv: [`--inspect=${debugPort}`, '--experimental-strip-types'],
+    execArgv: [`--inspect=${debugPort}`, '--experimental-vm-modules', '--experimental-strip-types'],
   })
 
-  const root = getRoot()
-  const rootPath = fileURLToPath(root)
-  // TODO file path has duplicate slash for some reason, it should only have one slash or backslash.
-  const slash = process.platform === 'win32' ? '\\' : '/'
-  const filePath = `${rootPath}${slash}any-file.txt`
+  await new Promise((r) => {
+    setTimeout(r, 1000)
+  })
 
-  console.log({ filePath })
+  const client = await CDP({
+    host: 'localhost',
+    port: Number(debugPort),
+  })
+  await client.Runtime.enable()
 
-  await previewProcess.invoke('Test.mockFs', 'node:fs/promises', 'readFile', filePath, 'Access Denied', 'EACCES')
+  await client.Runtime.evaluate({
+    expression: `
+      (async () => {
+        const fs = await import('node:fs/promises')
+        const originalReadFile = fs.readFile
+        fs.readFile = () => {
+          const error = new Error('EACCES: permission denied')
+          error.code = 'EACCES'
+          throw error
+        }
+      })()
+    `,
+    awaitPromise: true,
+  })
 
   const id = 1
   const port = await getPort()
+  const root = getRoot()
 
   await previewProcess.invoke('WebViewServer.create', id)
   await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '')
@@ -32,5 +48,6 @@ test('preview process - internal server error', async () => {
   expect(response.status).toBe(500)
   expect(await response.text()).toBe('Internal Server Error')
 
+  await client.close()
   previewProcess[Symbol.dispose]()
 })
