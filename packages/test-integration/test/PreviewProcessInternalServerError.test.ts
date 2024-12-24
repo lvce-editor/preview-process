bf54f4678fd77f843a2e71db2f97b8245bd71319
Le Vivilet <levivlet@pm.me>
2024-12-24 18:46:46 +0100

    feature: improve server error test

--- packages/test-integration/test/PreviewProcessInternalServerError.test.ts
+++ packages/test-integration/test/PreviewProcessInternalServerError.test.ts
@@ -3,9 +3,42 @@ import getPort from 'get-port'
 import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
 import { get } from '../src/parts/Get/Get.js'
 import { getRoot } from '../src/parts/GetRoot/GetRoot.js'
+import { WebSocket } from 'ws'
 
-test.skip('preview process - internal server error', async () => {
-  const previewProcess = createPreviewProcess()
+test('preview process - internal server error', async () => {
+  const debugPort = await getPort()
+  const previewProcess = createPreviewProcess({
+    execArgv: [`--inspect=${debugPort}`, '--experimental-strip-types'],
+  })
+
+  // Get WebSocket URL from Chrome DevTools Protocol
+  const response = await get(`http://localhost:${debugPort}/json/list`)
+  const data = await response.json()
+  const wsUrl = data[0].webSocketDebuggerUrl
+
+  // Connect to debug websocket
+  const ws = new WebSocket(wsUrl)
+  await new Promise((resolve) => ws.once('open', resolve))
+
+  // Send runtime.evaluate to override fs.readFile
+  const r = await new Promise((resolve) => {
+    ws.send(
+      JSON.stringify({
+        id: 1,
+        method: 'Runtime.evaluate',
+        params: {
+          expression: `
+          import { promises as fs } from 'node:fs'
+          fs.readFile = () => { throw new Error('Simulated internal error') }
+        `,
+        },
+      }),
+    )
+    ws.once('message', resolve)
+  })
+  // @ts-ignore
+  console.log({ r: r.toString() })
+
   const id = 1
   const port = await getPort()
   const root = getRoot()
@@ -14,10 +47,10 @@ test.skip('preview process - internal server error', async () => {
   await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '')
   await previewProcess.invoke('WebViewServer.start', id, port)
 
-  // Using a special path that triggers internal server error in the preview process
-  const response = await get(`http://localhost:${port}/__internal_error__`)
-  expect(response.status).toBe(500)
-  expect(await response.text()).toBe('500 - Internal Server Error')
+  const response2 = await get(`http://localhost:${port}/any-file.txt`)
+  expect(response2.status).toBe(500)
+  expect(await response2.text()).toBe('[preview-server] Error: Simulated internal error')
 
+  ws.close()
   previewProcess[Symbol.dispose]()
 })
