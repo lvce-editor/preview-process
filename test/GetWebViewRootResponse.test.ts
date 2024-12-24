f7152c78fe64b0c353b24f1608b584b9d440cd7a
Le Vivilet <levivlet@pm.me>
2024-12-24 08:57:16 +0100

    feature: add webviewRootResponse test

---
+++ test/GetWebViewRootResponse.test.ts
@@ -0,0 +1,64 @@
+import { expect, jest, test } from '@jest/globals'
+import * as ErrorCodes from '../src/parts/ErrorCodes/ErrorCodes.ts'
+import type { Info } from '../src/parts/Info.ts'
+
+jest.unstable_mockModule('../src/parts/FileSystem/FileSystem.ts', () => {
+  return {
+    readFile: jest.fn(),
+  }
+})
+
+const GetWebViewRootResponse = await import('../src/parts/GetWebViewRootResponse/GetWebViewRootResponse.ts')
+const FileSystem = await import('../src/parts/FileSystem/FileSystem.ts')
+
+const mockInfo: Info = {
+  webViewId: 'test',
+  webViewRoot: '/test',
+  contentSecurityPolicy: '',
+  iframeContent: '',
+}
+
+test('getWebViewRootResponse - file not found', async () => {
+  const error = new Error('ENOENT: no such file')
+  // @ts-ignore
+  error.code = ErrorCodes.ENOENT
+  jest.spyOn(FileSystem, 'readFile').mockRejectedValue(error)
+
+  const response = await GetWebViewRootResponse.getWebViewRootResponse(mockInfo, '/not-found.txt')
+  expect(response).toEqual({
+    body: 'not found',
+    init: {
+      status: 404,
+    },
+  })
+})
+
+test('getWebViewRootResponse - internal server error', async () => {
+  const error = new Error('Unknown error')
+  jest.spyOn(FileSystem, 'readFile').mockRejectedValue(error)
+
+  const response = await GetWebViewRootResponse.getWebViewRootResponse(mockInfo, '/error.txt')
+  expect(response).toEqual({
+    body: 'Internal Server Error',
+    init: {
+      status: 500,
+    },
+  })
+})
+
+test('getWebViewRootResponse - success', async () => {
+  jest.spyOn(FileSystem, 'readFile').mockResolvedValue(Buffer.from('test content'))
+
+  const response = await GetWebViewRootResponse.getWebViewRootResponse(mockInfo, '/test.txt')
+  expect(response).toEqual({
+    body: Buffer.from('test content'),
+    init: {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/plain',
+        'Cross-Origin-Resource-Policy': 'same-origin',
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+      },
+    },
+  })
+})
