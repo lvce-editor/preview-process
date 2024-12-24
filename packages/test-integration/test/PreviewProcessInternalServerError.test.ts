2a85bdd2b4f0579d8cb8030691a3904eb6fbf033
Le Vivilet <levivlet@pm.me>
2024-12-24 23:03:45 +0100

    use cdp

--- packages/test-integration/test/PreviewProcessInternalServerError.test.ts
+++ packages/test-integration/test/PreviewProcessInternalServerError.test.ts
@@ -2,57 +2,38 @@ import { expect, test } from '@jest/globals'
 import getPort from 'get-port'
 import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
 import { get } from '../src/parts/Get/Get.js'
-import * as WebSocket from '../src/parts/WebSocket/WebSocket.js'
 import { getRoot } from '../src/parts/GetRoot/GetRoot.js'
+import CDP from 'chrome-remote-interface'
 
 test('preview process - internal server error', async () => {
   const debugPort = await getPort()
   const previewProcess = createPreviewProcess({
-    execArgv: [`--inspect=${debugPort}`, '--experimental-strip-types'],
+    execArgv: [`--inspect=${debugPort}`],
   })
 
-  // Get WebSocket URL from Chrome DevTools Protocol
-  const response = await get(`http://localhost:${debugPort}/json/list`)
-  const data = await response.json()
-  const wsUrl = data[0].webSocketDebuggerUrl
-
-  // Connect to debug websocket
-  const ws = await WebSocket.connect(wsUrl)
-
-  // Enable Runtime domain
-  await WebSocket.invoke(ws, 'Runtime.enable', {})
+  const client = await CDP({ port: debugPort })
+  await client.Runtime.enable()
 
   // First compile the script
-  const rr1 = await WebSocket.invoke(ws, 'Runtime.compileScript', {
+  await client.Runtime.compileScript({
     expression: `
-      const fs = require('fs/promises');
+      const fs = require('node:fs/promises')
+      const originalReadFile = fs.readFile
       fs.readFile = () => {
-        const error = new Error('EACCES: permission denied');
-        error.code = 'EACCES';
-        throw error;
+        const error = new Error('EACCES: permission denied')
+        error.code = 'EACCES'
+        throw error
       }
-        throw new Error('oops')
     `,
     sourceURL: 'test.js',
     persistScript: true,
-    scriptId: 'my-script',
+    executionContextId: 1,
   })
 
-  console.log({ rr1 })
   // Then run it
-  const rr = await WebSocket.invoke(ws, 'Runtime.runScript', {
-    scriptId: 'my-script',
-    awaitPromise: true,
+  await client.Runtime.runScript({
+    scriptId: '1',
   })
-
-  console.log({ rr })
-
-  const rr2 = await WebSocket.invoke(ws, 'Runtime.evaluate', {
-    expression: 'typeof globalThis.abc',
-    returnByValue: true,
-  })
-
-  console.log({ rr, rr2 })
 
   const id = 1
   const port = await getPort()
@@ -62,10 +43,10 @@ test('preview process - internal server error', async () => {
   await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '')
   await previewProcess.invoke('WebViewServer.start', id, port)
 
-  const response2 = await get(`http://localhost:${port}/any-file.txt`)
-  expect(response2.status).toBe(500)
-  expect(await response2.text()).toBe('[preview-server] Error: EACCES: permission denied')
+  const response = await get(`http://localhost:${port}/any-file.txt`)
+  expect(response.status).toBe(500)
+  expect(await response.text()).toBe('[preview-server] Error: EACCES: permission denied')
 
-  WebSocket.dispose(ws)
+  await client.close()
   previewProcess[Symbol.dispose]()
 })
