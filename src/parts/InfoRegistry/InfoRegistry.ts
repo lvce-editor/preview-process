299784b981c43b794f228e4caa586b86c4f657f9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-24 08:33:20 +0100

    feature: simplify state (#118)

--- src/parts/InfoRegistry/InfoRegistry.ts
+++ src/parts/InfoRegistry/InfoRegistry.ts
@@ -4,14 +4,12 @@ interface State {
   [key: string]: Info
 }
 
-export const state: State = {
-  infos: Object.create(null),
-}
+const infos: State = Object.create(null)
 
 export const set = (webViewId: string, info: Info): void => {
-  state[webViewId] = info
+  infos[webViewId] = info
 }
 
 export const get = (webViewId: string): Info => {
-  return state[webViewId]
+  return infos[webViewId]
 }
