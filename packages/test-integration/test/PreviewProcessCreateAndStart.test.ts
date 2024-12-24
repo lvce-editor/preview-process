f6ae3d58d394e8190786813d87d0cbce16c1aaff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-24 16:07:38 +0100

    feature: add csp integration test (#144)

--- packages/test-integration/test/PreviewProcessCreateAndStart.test.ts
+++ packages/test-integration/test/PreviewProcessCreateAndStart.test.ts
@@ -8,13 +8,15 @@ test('preview process - create and start server', async () => {
   const id = 1
   const port = await getPort()
   const root = new URL('../../../', import.meta.url)
+  const csp = "default-src 'self'; script-src 'self'"
 
   await previewProcess.invoke('WebViewServer.create', id)
-  await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '<h1>Hello World</h1>')
+  await previewProcess.invoke('WebViewServer.setHandler', id, '', root, csp, '<h1>Hello World</h1>')
   await previewProcess.invoke('WebViewServer.start', id, port)
 
   const response = await get(`http://localhost:${port}`)
   expect(response.status).toBe(200)
+  expect(response.headers.get('content-security-policy')).toBe(csp)
   expect(await response.text()).toBe('<h1>Hello World</h1>')
 
   previewProcess[Symbol.dispose]()
