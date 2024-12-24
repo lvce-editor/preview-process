6a008533e2031c7a7515cf3635c2f80cd6bc8005
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-24 12:31:23 +0100

    feature: add resolve tests (#127)
    * feature: add resolve tests
    
    * windows
    
    * test

--- test/ResolveFilePath.test.ts
+++ test/ResolveFilePath.test.ts
@@ -1,39 +1,23 @@
 import { expect, test } from '@jest/globals'
-import { join } from 'node:path'
-import * as ResolveFilePath from '../src/parts/ResolveFilePath/ResolveFilePath.js'
+import * as ResolveFilePath from '../src/parts/ResolveFilePath/ResolveFilePath.ts'
 
-test.skip('resolveFilePath - normal path', () => {
+test('resolveFilePath - remote path', () => {
+  const pathName = process.platform === 'win32' ? '/remote/C:/test/file.txt' : '/remote//test/file.txt'
+  const webViewRoot = '/root'
+  const expected = process.platform === 'win32' ? 'C:\\test\\file.txt' : '/test/file.txt'
+  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe(expected)
+})
+
+test.skip('resolveFilePath - remote path with double slash', () => {
+  const pathName = process.platform === 'win32' ? '/remote//C:/test/file.txt' : '/remote///test/file.txt'
+  const webViewRoot = '/root'
+  const expected = process.platform === 'win32' ? 'C:\\test\\file.txt' : '/test/file.txt'
+  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe(expected)
+})
+
+test('resolveFilePath - normal path', () => {
   const pathName = '/test/file.txt'
-  const webViewRoot = '/root'
-  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe(join('/root', 'test/file.txt'))
-})
-
-test.skip('resolveFilePath - path with dots', () => {
-  const pathName = '/test/../file.txt'
-  const webViewRoot = '/root'
-  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe(join('/root', 'file.txt'))
-})
-
-test.skip('resolveFilePath - empty path', () => {
-  const pathName = ''
-  const webViewRoot = '/root'
-  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe('/root')
-})
-
-test.skip('resolveFilePath - root path', () => {
-  const pathName = '/'
-  const webViewRoot = '/root'
-  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe('/root/')
-})
-
-test.skip('resolveFilePath - path with query parameters', () => {
-  const pathName = '/test/file.txt?query=value'
-  const webViewRoot = '/root'
-  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe(join('/root', 'test/file.txt'))
-})
-
-test.skip('resolveFilePath - path with hash', () => {
-  const pathName = '/test/file.txt#section'
-  const webViewRoot = '/root'
-  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe(join('/root', 'test/file.txt'))
+  const webViewRoot = process.platform === 'win32' ? 'C:/root' : '/root'
+  const expected = process.platform === 'win32' ? 'C:\\root\\test\\file.txt' : '/root/test/file.txt'
+  expect(ResolveFilePath.resolveFilePath(pathName, webViewRoot)).toBe(expected)
 })
