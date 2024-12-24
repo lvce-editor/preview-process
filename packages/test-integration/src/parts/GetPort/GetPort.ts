f1e11c703b1335c50a148364e3f0e622e19d2b27
Le Vivilet <levivlet@pm.me>
2024-12-24 15:51:58 +0100

    feature: use getPort function instead of hardcoding port

---
+++ packages/test-integration/src/parts/GetPort/GetPort.ts
@@ -0,0 +1,5 @@
+import getPortModule from 'get-port'
+
+export const getPort = () => {
+  return getPortModule()
+}
--- packages/test-integration/test/PreviewProcessCreateAndStart.test.ts
+++ packages/test-integration/test/PreviewProcessCreateAndStart.test.ts
@@ -1,18 +1,19 @@
 import { expect, test } from '@jest/globals'
-import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
-import { get } from '../src/parts/Get/Get.js'
+import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.ts'
+import { get } from '../src/parts/Get/Get.ts'
+import { getPort } from '../src/parts/GetPort/GetPort.ts'
 
-test.skip('preview process - create and start server', async () => {
+test('preview process - create and start server', async () => {
   const previewProcess = createPreviewProcess()
   const id = 1
-  const port = '3000'
+  const port = await getPort()
 
   await previewProcess.invoke('WebViewServer.create', id)
   await previewProcess.invoke('WebViewServer.setInfo', id, 'test', '/test', '', '<h1>Hello World</h1>')
   await previewProcess.invoke('WebViewServer.setHandler', id, '', '/test', '', '<h1>Hello World</h1>')
-  await previewProcess.invoke('WebViewServer.start', id, port)
+  await previewProcess.invoke('WebViewServer.start', id, port.toString())
 
-  const response = await get('http://localhost:3000')
+  const response = await get(`http://localhost:${port}`)
   expect(response.status).toBe(200)
   expect(await response.text()).toBe('<h1>Hello World</h1>')
 
