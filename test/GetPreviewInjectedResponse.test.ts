3b6e249ce95fb5d4c773e4aa41879a5a65be3368
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-24 11:30:00 +0100

    feature: add preview injected code test (#128)
    * feature: add preview injected code test
    
    * test

---
+++ test/GetPreviewInjectedResponse.test.ts
@@ -0,0 +1,24 @@
+import { expect, jest, test } from '@jest/globals'
+
+jest.unstable_mockModule('../src/parts/PreviewInjectedCode/PreviewInjectedCode.ts', () => {
+  return {
+    injectedCode: 'console.log("mocked code")',
+  }
+})
+
+const GetPreviewInjectedResponse = await import('../src/parts/GetPreviewInjectedResponse/GetPreviewInjectedResponse.ts')
+
+test('getPreviewInjectedResponse', async () => {
+  const response = await GetPreviewInjectedResponse.getPreviewInjectedResponse()
+
+  expect(response).toEqual({
+    body: 'console.log("mocked code")',
+    init: {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/javascript',
+        'Cross-Origin-Resource-Policy': 'same-origin',
+      },
+    },
+  })
+})
