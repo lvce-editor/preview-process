6fec0c97e7b8663d3b61f68a2d79cb374d8318bb
Le Vivilet <levivlet@pm.me>
2024-12-24 23:25:56 +0100

    mock fs

--- packages/preview-process/src/parts/MockFs/MockFs.ts
+++ packages/preview-process/src/parts/MockFs/MockFs.ts
@@ -1,4 +1,26 @@
-export const mockFs = () => {
-  // TODO
-  throw new Error('mockfs called')
+export const mockFs = async () => {
+  const m = await import('node:module')
+
+  const require = m.default.createRequire(import.meta.url)
+
+  const fs = require('node:fs/promises')
+
+  const mock = () => {
+    throw new Error('oops')
+  }
+  fs.writeFile = mock
+
+  const rr1 = fs.writeFile === mock
+
+  m.syncBuiltinESMExports()
+  // later
+
+  const rr = await (async () => {
+    const fs = await import('node:fs/promises')
+    await fs.writeFile('/tmp/abc.txt', 'abc')
+    console.log(fs.writeFile)
+    return fs.writeFile === mock
+  })()
+
+  return `${rr} ${rr1}`
 }
