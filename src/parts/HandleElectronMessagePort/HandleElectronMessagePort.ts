bc36db3e1f57d281a067b3a1adf9d3b4463bb768
Le Vivilet <levivlet@pm.me>
2024-12-24 08:44:08 +0100

    fix: cyclic dependency in handleElectronMessagePort

--- src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
+++ src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
@@ -1,9 +1,13 @@
 import { ElectronMessagePortRpcClient } from '@lvce-editor/rpc'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as WebViewProtocol from '../WebViewProtocol/WebViewProtocol.ts'
+
+const commandMap = {
+  'WebViewProtocol.getResponse': WebViewProtocol.getResponse,
+}
 
 export const handleElectronMessagePort = async (messagePort: any): Promise<void> => {
   await ElectronMessagePortRpcClient.create({
     messagePort,
-    commandMap: CommandMap.commandMap,
+    commandMap,
   })
 }
