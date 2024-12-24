b38e7f887e0f1985e5646fbccce5405dcd03f54b
Le Vivilet <levivlet@pm.me>
2024-12-24 15:40:33 +0100

    fix

--- packages/test-integration/src/parts/Get/Get.ts
+++ packages/test-integration/src/parts/Get/Get.ts
@@ -1,6 +1,6 @@
 import ky from 'ky'
 
-export const get = async (url: string) => {
+export const get = async (url: string): Promise<any> => {
   const response = await ky.get(url, {
     throwHttpErrors: false,
   })
