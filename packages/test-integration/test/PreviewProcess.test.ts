ef21a37de7bf77d06ba5846a819912237187b63e
Le Vivilet <levivlet@pm.me>
2024-12-24 15:26:08 +0100

    rename method

--- packages/test-integration/test/PreviewProcess.test.ts
+++ packages/test-integration/test/PreviewProcess.test.ts
@@ -7,7 +7,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const PREVIEW_PROCESS_PATH = join(__dirname, '../../preview-process/src/previewProcessMain.ts')
 
 interface PreviewProcess {
-  readonly send: (method: string, ...params: unknown[]) => Promise<unknown>
+  readonly invoke: (method: string, ...params: unknown[]) => Promise<unknown>
   readonly dispose: () => void
 }
 
@@ -16,7 +16,7 @@ const createPreviewProcess = (): PreviewProcess => {
     execArgv: ['--experimental-strip-types'],
   })
   return {
-    async send(method: string, ...params: any[]): Promise<void> {
+    async invoke(method: string, ...params: any[]): Promise<void> {
       const { promise, resolve } = Promise.withResolvers<any>()
       const messageId = Math.random()
       const listener = (message: any): void => {
@@ -51,16 +51,16 @@ test('preview process - create and start server', async () => {
   const port = '3000'
 
   // Create server
-  await previewProcess.send('WebViewServer.create', id)
+  await previewProcess.invoke('WebViewServer.create', id)
 
   // Set server info
-  await previewProcess.send('WebViewServer.setInfo', id, 'test', '/test', '', '<h1>Hello World</h1>')
+  await previewProcess.invoke('WebViewServer.setInfo', id, 'test', '/test', '', '<h1>Hello World</h1>')
 
   // Set handler
-  await previewProcess.send('WebViewServer.setHandler', id, '', '/test', '', '<h1>Hello World</h1>')
+  await previewProcess.invoke('WebViewServer.setHandler', id, '', '/test', '', '<h1>Hello World</h1>')
 
   // Start server
-  await previewProcess.send('WebViewServer.start', id, port)
+  await previewProcess.invoke('WebViewServer.start', id, port)
 
   // Make request to server
   const response = await get('http://localhost:3000')
@@ -75,10 +75,10 @@ test('preview process - serve static files', async () => {
   const id = 1
   const port = '3001'
 
-  await previewProcess.send('WebViewServer.create', id)
-  await previewProcess.send('WebViewServer.setInfo', id, 'test', process.cwd(), '', '')
-  await previewProcess.send('WebViewServer.setHandler', id, '', process.cwd(), '', '')
-  await previewProcess.send('WebViewServer.start', id, port)
+  await previewProcess.invoke('WebViewServer.create', id)
+  await previewProcess.invoke('WebViewServer.setInfo', id, 'test', process.cwd(), '', '')
+  await previewProcess.invoke('WebViewServer.setHandler', id, '', process.cwd(), '', '')
+  await previewProcess.invoke('WebViewServer.start', id, port)
 
   const response = await get('http://localhost:3001/package.json')
   expect(response.status).toBe(200)
@@ -95,10 +95,10 @@ test('preview process - 404 for non-existent files', async () => {
   const id = 1
   const port = '3002'
 
-  await previewProcess.send('WebViewServer.create', id)
-  await previewProcess.send('WebViewServer.setInfo', id, 'test', process.cwd(), '', '')
-  await previewProcess.send('WebViewServer.setHandler', id, '', process.cwd(), '', '')
-  await previewProcess.send('WebViewServer.start', id, port)
+  await previewProcess.invoke('WebViewServer.create', id)
+  await previewProcess.invoke('WebViewServer.setInfo', id, 'test', process.cwd(), '', '')
+  await previewProcess.invoke('WebViewServer.setHandler', id, '', process.cwd(), '', '')
+  await previewProcess.invoke('WebViewServer.start', id, port)
 
   const response = await get('http://localhost:3002/non-existent-file.txt')
   expect(response.status).toBe(404)
