30a30b86337ebc24a6bf1d3328a1ac6d014cafc0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-24 15:54:25 +0100

    feature: enable createAndStart test (#139)
    * feature: enable createAndStart test
    
    * lint

--- packages/test-integration/test/PreviewProcessCreateAndStart.test.ts
+++ packages/test-integration/test/PreviewProcessCreateAndStart.test.ts
@@ -1,15 +1,17 @@
 import { expect, test } from '@jest/globals'
+import { fileURLToPath } from 'node:url'
 import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
 import { get } from '../src/parts/Get/Get.js'
 
-test.skip('preview process - create and start server', async () => {
+test('preview process - create and start server', async () => {
   const previewProcess = createPreviewProcess()
   const id = 1
   const port = '3000'
+  const root = fileURLToPath(new URL('../test', import.meta.url))
 
   await previewProcess.invoke('WebViewServer.create', id)
-  await previewProcess.invoke('WebViewServer.setInfo', id, 'test', '/test', '', '<h1>Hello World</h1>')
-  await previewProcess.invoke('WebViewServer.setHandler', id, '', '/test', '', '<h1>Hello World</h1>')
+  await previewProcess.invoke('WebViewServer.setInfo', id, 'test', root, '', '<h1>Hello World</h1>')
+  await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '<h1>Hello World</h1>')
   await previewProcess.invoke('WebViewServer.start', id, port)
 
   const response = await get('http://localhost:3000')
