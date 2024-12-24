91d4bfef918d334014618a35e6e29098513066fa
Le Vivilet <levivlet@pm.me>
2024-12-24 22:54:40 +0100

    debug fs

--- packages/test-integration/test/PreviewProcessInternalServerError.test.ts
+++ packages/test-integration/test/PreviewProcessInternalServerError.test.ts
@@ -1,14 +1,14 @@
-import { expect, test } from '@jest/globals'
+import { test } from '@jest/globals'
 import getPort from 'get-port'
 import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
 import { get } from '../src/parts/Get/Get.js'
 import * as WebSocket from '../src/parts/WebSocket/WebSocket.js'
-import { getRoot } from '../src/parts/GetRoot/GetRoot.js'
 
 test('preview process - internal server error', async () => {
   const debugPort = await getPort()
+  // @ts-ignore
   const previewProcess = createPreviewProcess({
-    execArgv: [`--inspect=${debugPort}`],
+    execArgv: [`--inspect=${debugPort}`, '--experimental-strip-types'],
   })
 
   // Get WebSocket URL from Chrome DevTools Protocol
@@ -19,38 +19,41 @@ test('preview process - internal server error', async () => {
   // Connect to debug websocket
   const ws = await WebSocket.connect(wsUrl)
 
-  // Override fs.readFile to simulate EACCES error
-  const x = await WebSocket.invoke(ws, 'Runtime.evaluate', {
-    expression: `(async ()=>{
-    // const fs = await import('fs')
-    return [...process.execArgv]
-  })()
-    //const fs = await import('node:fs/promises')
-    // const originalReadFile = fs.readFile
-    // fs.readFile = () => {
-    //   const error = new Error('EACCES: permission denied')
-    //   error.code = 'EACCES'
-    //   throw error
-    // }
+  // First compile the script
+  await WebSocket.invoke(ws, 'Runtime.compileScript', {
+    expression: `
+      const fs = require('fs/promises');
+      fs.readFile = () => {
+        const error = new Error('EACCES: permission denied');
+        error.code = 'EACCES';
+        throw error;
+      }
     `,
-    awaitPromise: true,
-    returnByValue: true,
+    sourceURL: 'test.js',
+    persistScript: true,
+    scriptId: 'my-script',
   })
 
-  console.log({ x })
+  // Then run it
+  const rr = await WebSocket.invoke(ws, 'Runtime.runScript', {
+    scriptId: 'my-script',
+    awaitPromise: true,
+  })
 
-  const id = 1
-  const port = await getPort()
-  const root = getRoot()
+  console.log({ rr })
 
-  await previewProcess.invoke('WebViewServer.create', id)
-  await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '')
-  await previewProcess.invoke('WebViewServer.start', id, port)
+  // const id = 1
+  // const port = await getPort()
+  // const root = getRoot()
 
-  const response2 = await get(`http://localhost:${port}/any-file.txt`)
-  expect(response2.status).toBe(500)
-  expect(await response2.text()).toBe('[preview-server] Error: EACCES: permission denied')
+  // await previewProcess.invoke('WebViewServer.create', id)
+  // await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '')
+  // await previewProcess.invoke('WebViewServer.start', id, port)
 
-  WebSocket.dispose(ws)
-  previewProcess[Symbol.dispose]()
+  // const response2 = await get(`http://localhost:${port}/any-file.txt`)
+  // expect(response2.status).toBe(500)
+  // expect(await response2.text()).toBe('[preview-server] Error: EACCES: permission denied')
+
+  // WebSocket.dispose(ws)
+  // previewProcess[Symbol.dispose]()
 })
