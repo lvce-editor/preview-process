6d6b97d88853f35380566f6a074b1ced7f4b55ec
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 14:26:20 +0100

    Discard changes to packages/preview-process/src/parts/RequestOptions/RequestOptions.ts

--- packages/preview-process/src/parts/RequestOptions/RequestOptions.ts
+++ packages/preview-process/src/parts/RequestOptions/RequestOptions.ts
@@ -1,7 +1,5 @@
 export interface RequestOptions {
   readonly method: string
-  // TODO this is part of headers already
   readonly range?: string
   readonly path: string
-  readonly headers: any
 }
