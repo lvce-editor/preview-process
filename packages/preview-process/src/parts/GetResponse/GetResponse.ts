d437c76b777649617888613494c54e0c9e4cf022
Le Vivilet <levivlet@pm.me>
2024-12-25 14:08:01 +0100

    feature: simplify getResponse function

--- packages/preview-process/src/parts/GetResponse/GetResponse.ts
+++ packages/preview-process/src/parts/GetResponse/GetResponse.ts
@@ -1,27 +1,17 @@
 import type { HandlerOptions } from '../HandlerOptions/HandlerOptions.ts'
 import type { RequestOptions } from '../RequestOptions/RequestOptions.ts'
-import * as HandleIndexHtml from '../HandleIndexHtml/HandleIndexHtml.ts'
-import * as HandleOther from '../HandleOther/HandleOther.ts'
-import * as HandlePreviewInjected from '../HandlePreviewInjected/HandlePreviewInjected.ts'
+import * as FindMatchingRoute from '../FindMatchingRoute/FindMatchingRoute.ts'
 import * as HttpMethod from '../HttpMethod/HttpMethod.ts'
-import * as ResolveFilePath from '../ResolveFilePath/ResolveFilePath.ts'
 import { HeadResponse } from '../Responses/HeadResponse.ts'
 import { MethodNotAllowedResponse } from '../Responses/MethodNotAllowedResponse.ts'
+import * as Routes from '../Routes/Routes.ts'
 
 export const getResponse = async (request: RequestOptions, options: HandlerOptions): Promise<Response> => {
   if (request.method !== HttpMethod.Get && request.method !== HttpMethod.Head) {
     return new MethodNotAllowedResponse()
   }
-  const filePath = ResolveFilePath.resolveFilePath(request.path, options.webViewRoot)
-  const isHtml = filePath.endsWith('index.html')
-  let response
-  if (isHtml) {
-    response = await HandleIndexHtml.handleIndexHtml(filePath, options)
-  } else if (filePath.endsWith('preview-injected.js')) {
-    response = await HandlePreviewInjected.handlePreviewInjected()
-  } else {
-    response = await HandleOther.handleOther(filePath, request)
-  }
+  const matchedRoute = FindMatchingRoute.findMatchingRoute(request.path, Routes.routes)
+  const response = await matchedRoute.handler(request, options)
   if (request.method === HttpMethod.Head) {
     return new HeadResponse(response.status, response.headers)
   }
