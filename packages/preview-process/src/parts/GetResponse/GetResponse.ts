e426688efaef9a70943985c3eae96564f486f9f7
Le Vivilet <levivlet@pm.me>
2024-12-25 12:40:59 +0100

    feature: simplify getResponse function

--- packages/preview-process/src/parts/GetResponse/GetResponse.ts
+++ packages/preview-process/src/parts/GetResponse/GetResponse.ts
@@ -1,3 +1,5 @@
+import type { HandlerOptions } from '../HandlerOptions/HandlerOptions.ts'
+import type { RequestOptions } from '../RequestOptions/RequestOptions.ts'
 import * as HandleIndexHtml from '../HandleIndexHtml/HandleIndexHtml.ts'
 import * as HandleOther from '../HandleOther/HandleOther.ts'
 import * as HandlePreviewInjected from '../HandlePreviewInjected/HandlePreviewInjected.ts'
@@ -5,31 +7,23 @@ import * as HttpMethod from '../HttpMethod/HttpMethod.ts'
 import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.ts'
 import * as ResolveFilePath from '../ResolveFilePath/ResolveFilePath.ts'
 
-export const getResponse = async (
-  pathName: string,
-  webViewRoot: string,
-  contentSecurityPolicy: string,
-  iframeContent: string,
-  range: any,
-  method: string | undefined,
-): Promise<Response> => {
-  if (method !== HttpMethod.Get && method !== HttpMethod.Head) {
+export const getResponse = async (request: RequestOptions, options: HandlerOptions): Promise<Response> => {
+  if (request.method !== HttpMethod.Get && request.method !== HttpMethod.Head) {
     return new Response('Method Not Allowed', {
       status: HttpStatusCode.MethodNotAllowed,
     })
   }
-  const filePath = ResolveFilePath.resolveFilePath(pathName, webViewRoot)
+  const filePath = ResolveFilePath.resolveFilePath(request.path, options.webViewRoot)
   const isHtml = filePath.endsWith('index.html')
   let response
   if (isHtml) {
-    response = await HandleIndexHtml.handleIndexHtml(filePath, contentSecurityPolicy, iframeContent)
+    response = await HandleIndexHtml.handleIndexHtml(filePath, options.contentSecurityPolicy, options.iframeContent)
   } else if (filePath.endsWith('preview-injected.js')) {
     response = await HandlePreviewInjected.handlePreviewInjected()
   } else {
-    response = await HandleOther.handleOther(filePath, range)
+    response = await HandleOther.handleOther(filePath, request.range)
   }
-
-  if (method === HttpMethod.Head) {
+  if (request.method === HttpMethod.Head) {
     return new Response(null, {
       status: response.status,
       headers: response.headers,
