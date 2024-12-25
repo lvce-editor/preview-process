33f35621e700c60871cace5f2977e4b85f200828
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 15:06:22 +0100

    feature: don't handle enoent error twice (#212)

--- packages/preview-process/src/parts/GetPathEtag/GetPathEtag.ts
+++ packages/preview-process/src/parts/GetPathEtag/GetPathEtag.ts
@@ -2,20 +2,11 @@ import { stat } from 'node:fs/promises'
 import * as GetEtag from '../GetEtag/GetEtag.ts'
 
 export const getPathEtag = async (absolutePath: string): Promise<string> => {
-  try {
-    let stats = await stat(absolutePath)
-    if (stats.isDirectory()) {
-      absolutePath += '/index.html'
-      stats = await stat(absolutePath)
-    }
-    const etag = GetEtag.getEtag(stats)
-    return etag
-  } catch (error) {
-    // @ts-ignore
-    if (error && error.code === 'ENOENT') {
-      return ''
-    }
-    console.error(`[static-server] etag error ${error}`)
-    return ''
+  let stats = await stat(absolutePath)
+  if (stats.isDirectory()) {
+    absolutePath += '/index.html'
+    stats = await stat(absolutePath)
   }
+  const etag = GetEtag.getEtag(stats)
+  return etag
 }
