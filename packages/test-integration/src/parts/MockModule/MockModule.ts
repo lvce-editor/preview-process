1179c11bc95460ae6d7fa2f966fa7c49deba26ca
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 14:48:11 +0100

    feature: add options to mockmodule (#210)
    * feature: add options to mockmodule
    
    * simplify code

--- packages/test-integration/src/parts/MockModule/MockModule.ts
+++ packages/test-integration/src/parts/MockModule/MockModule.ts
@@ -1,22 +1,17 @@
 import type CDP from 'chrome-remote-interface'
 
-export const mockModule = async (
-  runtime: CDP.Client['Runtime'],
-  moduleName: string,
-  key: string,
-  arg0: string,
-  errorMessage: string,
-  errorCode: string,
-): Promise<void> => {
-  const global = await runtime.evaluate({
-    expression: 'globalThis',
-  })
-  await runtime.callFunctionOn({
-    objectId: global.result.objectId,
-    functionDeclaration: `function(moduleName, key, arg0, errorMessage, errorCode){
-      const global = this
-      global.mockModule(moduleName, key, arg0, errorMessage, errorCode)
-    }`,
-    arguments: [{ value: moduleName }, { value: key }, { value: arg0 }, { value: errorMessage }, { value: errorCode }],
+interface MockOptions {
+  readonly moduleName: string
+  readonly functionName: string
+  readonly filePath?: string
+  readonly returnValue?: any
+  readonly errorMessage?: string
+  readonly errorCode?: string
+}
+
+export const mockModule = async (runtime: CDP.Client['Runtime'], options: MockOptions): Promise<void> => {
+  await runtime.evaluate({
+    expression: `mockModule('${options.moduleName}', ${JSON.stringify(options)})`,
+    awaitPromise: true,
   })
 }
