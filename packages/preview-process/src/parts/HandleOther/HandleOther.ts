8b4f54e09e306402eac1cb162a29a16d3c9c5743
Le Vivilet <levivlet@pm.me>
2024-12-25 13:47:51 +0100

    feature: use serverErrorResponse

--- packages/preview-process/src/parts/HandleOther/HandleOther.ts
+++ packages/preview-process/src/parts/HandleOther/HandleOther.ts
@@ -3,8 +3,9 @@ import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetContentType from '../GetContentType/GetContentType.ts'
 import * as HandleRangeRequest from '../HandleRangeRequest/HandleRangeRequest.ts'
 import * as HttpHeader from '../HttpHeader/HttpHeader.ts'
-import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.ts'
 import * as IsEnoentError from '../IsEnoentError/IsEnoentError.ts'
+import { NotFoundResponse } from '../Responses/NotFoundResponse.ts'
+import { ServerErrorResponse } from '../Responses/ServerErrorResponse.ts'
 
 export const handleOther = async (filePath: string, requestOptions: RequestOptions): Promise<Response> => {
   try {
@@ -22,19 +23,9 @@ export const handleOther = async (filePath: string, requestOptions: RequestOptio
     })
   } catch (error) {
     if (IsEnoentError.isEnoentError(error)) {
-      return new Response('not found', {
-        status: HttpStatusCode.NotFound,
-        headers: {
-          [HttpHeader.CrossOriginResourcePolicy]: 'same-origin',
-        },
-      })
+      return new NotFoundResponse()
     }
     console.error(`[preview-server] ${error}`)
-    return new Response(`Internal Server Error`, {
-      status: HttpStatusCode.ServerError,
-      headers: {
-        [HttpHeader.CrossOriginResourcePolicy]: 'same-origin',
-      },
-    })
+    return new ServerErrorResponse()
   }
 }
