88bb8d3b021809f21f4fb6a8c2294ad19616cd90
Le Vivilet <levivlet@pm.me>
2024-12-25 14:03:41 +0100

    use readonly

--- packages/preview-process/src/parts/FindMatchingRoute/FindMatchingRoute.ts
+++ packages/preview-process/src/parts/FindMatchingRoute/FindMatchingRoute.ts
@@ -1,5 +1,5 @@
 import type { RouteHandler } from '../RouteHandler/RouteHandler.ts'
 
-export const findMatchingRoute = (path: string, routes: RouteHandler[]): RouteHandler => {
+export const findMatchingRoute = (path: string, routes: readonly RouteHandler[]): RouteHandler => {
   return routes.find((route) => (typeof route.pattern === 'string' ? path === route.pattern : route.pattern.test(path)))
 }
