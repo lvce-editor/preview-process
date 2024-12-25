3b336c0d0334fac72fd6110ba0198bf0b6248ff9
Le Vivilet <levivlet@pm.me>
2024-12-25 09:15:30 +0100

    feature: use waitPort function

--- packages/test-integration/src/parts/ConnectToCdp/ConnectToCdp.ts
+++ packages/test-integration/src/parts/ConnectToCdp/ConnectToCdp.ts
@@ -1,8 +1,12 @@
 import CDP from 'chrome-remote-interface'
-import { setTimeout } from 'timers/promises'
+import waitPort from 'wait-port'
 
 export const connectToCdp = async (debugPort: number): Promise<CDP.Client> => {
-  await setTimeout(1000)
+  await waitPort({
+    port: debugPort,
+    host: 'localhost',
+    output: 'silent',
+  })
   const client = await CDP({
     host: 'localhost',
     port: debugPort,
