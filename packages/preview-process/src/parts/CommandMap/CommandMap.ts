4ef916e88ed47cb94d3126c8bada499790a9f9e6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 11:04:54 +0100

    feature: remove unused code (#169)

--- packages/preview-process/src/parts/CommandMap/CommandMap.ts
+++ packages/preview-process/src/parts/CommandMap/CommandMap.ts
@@ -1,10 +1,8 @@
 import * as CreateWebViewServer from '../CreateWebViewServer/CreateWebViewServer.ts'
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
-import * as MockFs from '../MockFs/MockFs.ts'
 import * as SetInfo from '../SetInfo/SetInfo.ts'
 import * as SetWebViewServerHandler from '../SetWebViewServerHandler/SetWebViewServerHandler.ts'
 import * as StartWebViewServer from '../StartWebViewServer/StartWebViewServer.ts'
-import * as WebViewProtocol from '../WebViewProtocol/WebViewProtocol.ts'
 
 export const commandMap = {
   'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
@@ -12,8 +10,4 @@ export const commandMap = {
   'WebViewServer.setHandler': SetWebViewServerHandler.setWebViewServerHandler,
   'WebViewServer.start': StartWebViewServer.startWebViewServer,
   'WebViewServer.setInfo': SetInfo.setInfo,
-
-  // TODO if possible, try to enable this through the debugger to
-  // avoid extra test code inside source code
-  'Test.mockFs': MockFs.mockFs,
 }
