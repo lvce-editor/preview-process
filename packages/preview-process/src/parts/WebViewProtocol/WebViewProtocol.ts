24d2f880f5a03ea158371b46b615fec5f6048170
Le Vivilet <levivlet@pm.me>
2024-12-25 23:40:26 +0100

    use functions

--- packages/preview-process/src/parts/WebViewProtocol/WebViewProtocol.ts
+++ packages/preview-process/src/parts/WebViewProtocol/WebViewProtocol.ts
@@ -1,34 +1,9 @@
+import { ElectronResponse } from '../ElectronResponse/ElectronResponse.ts'
 import * as GetInfo from '../GetInfo/GetInfo.ts'
 import * as GetPathName from '../GetPathName/GetPathName.ts'
 import * as GetResponse from '../GetResponse/GetResponse.ts'
 import type { HandlerOptions } from '../HandlerOptions/HandlerOptions.ts'
-
-interface ElectronResponse {
-  readonly body: any
-  readonly init: {
-    readonly status: number
-    readonly headers: any
-  }
-}
-
-const serializeHeaders = (headers: Response['headers']): any => {
-  const serialized = Object.create(null)
-  for (const [key, value] of headers) {
-    serialized[key] = value
-  }
-  return serialized
-}
-
-const serializeResponse = async (response: Response): Promise<ElectronResponse> => {
-  const buffer = await response.arrayBuffer()
-  return {
-    body: buffer,
-    init: {
-      status: response.status,
-      headers: serializeHeaders(response.headers),
-    },
-  }
-}
+import { serializeResponse } from '../SerializeResponse/SerializeResponse.ts'
 
 export const getResponse = async (method: string, url: string): Promise<ElectronResponse> => {
   const info = GetInfo.getInfo(url)
