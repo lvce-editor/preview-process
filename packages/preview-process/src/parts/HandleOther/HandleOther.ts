f80fb5682a377a2e5ccb38bcb7a0eef12d32277a
Le Vivilet <levivlet@pm.me>
2024-12-25 13:55:15 +0100

    feature: use ContentResponse class

--- packages/preview-process/src/parts/HandleOther/HandleOther.ts
+++ packages/preview-process/src/parts/HandleOther/HandleOther.ts
@@ -2,8 +2,8 @@ import type { RequestOptions } from '../RequestOptions/RequestOptions.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetContentType from '../GetContentType/GetContentType.ts'
 import * as HandleRangeRequest from '../HandleRangeRequest/HandleRangeRequest.ts'
-import * as HttpHeader from '../HttpHeader/HttpHeader.ts'
 import * as IsEnoentError from '../IsEnoentError/IsEnoentError.ts'
+import { ContentResponse } from '../Responses/ContentResponse.ts'
 import { NotFoundResponse } from '../Responses/NotFoundResponse.ts'
 import { ServerErrorResponse } from '../Responses/ServerErrorResponse.ts'
 
@@ -13,14 +13,8 @@ export const handleOther = async (filePath: string, requestOptions: RequestOptio
       return await HandleRangeRequest.handleRangeRequest(filePath, requestOptions.range)
     }
     const contentType = GetContentType.getContentType(filePath)
-    // TODO figure out which of these headers are actually needed
     const content = await FileSystem.readFile(filePath)
-    return new Response(content, {
-      headers: {
-        [HttpHeader.CrossOriginResourcePolicy]: 'same-origin',
-        [HttpHeader.ContentType]: contentType,
-      },
-    })
+    return new ContentResponse(content, contentType)
   } catch (error) {
     if (IsEnoentError.isEnoentError(error)) {
       return new NotFoundResponse()
--- packages/preview-process/src/parts/HandlePreviewInjected/HandlePreviewInjected.ts
+++ packages/preview-process/src/parts/HandlePreviewInjected/HandlePreviewInjected.ts
@@ -1,16 +1,13 @@
 import * as GetContentType from '../GetContentType/GetContentType.ts'
 import * as PreviewInjectedCode from '../PreviewInjectedCode/PreviewInjectedCode.ts'
+import { ContentResponse } from '../Responses/ContentResponse.ts'
 import { NotFoundResponse } from '../Responses/NotFoundResponse.ts'
 
 export const handlePreviewInjected = async (): Promise<Response> => {
   try {
     const { injectedCode } = PreviewInjectedCode
     const contentType = GetContentType.getContentType('/test/file.js')
-    return new Response(injectedCode, {
-      headers: {
-        'Content-Type': contentType,
-      },
-    })
+    return new ContentResponse(injectedCode, contentType)
   } catch (error) {
     console.error(`[preview-server] ${error}`)
     return new NotFoundResponse()
