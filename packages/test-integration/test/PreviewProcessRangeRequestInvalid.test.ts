dc4e76984ef3fdb077d0065a6c70e195fd201bac
Le Vivilet <levivlet@pm.me>
2024-12-25 11:36:41 +0100

    feature: add invalid range request test

---
+++ packages/test-integration/test/PreviewProcessRangeRequestInvalid.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@jest/globals'
+import getPort from 'get-port'
+import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
+import { get } from '../src/parts/Get/Get.js'
+import { getRoot } from '../src/parts/GetRoot/GetRoot.js'
+
+test('preview process - handles invalid range request', async () => {
+  const previewProcess = createPreviewProcess()
+  const id = 1
+  const port = await getPort()
+  const root = getRoot()
+
+  await previewProcess.invoke('WebViewServer.create', id)
+  await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '')
+  await previewProcess.invoke('WebViewServer.start', id, port)
+
+  const response = await get(`http://localhost:${port}/package.json`, {
+    headers: {
+      Range: 'bytes=invalid-range',
+    },
+  })
+
+  expect(response.status).toBe(416) // Range Not Satisfiable
+  expect(response.headers.get('Content-Range')).toMatch(/^bytes \*\/\d+$/)
+
+  previewProcess[Symbol.dispose]()
+})
