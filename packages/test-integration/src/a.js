f21b86b6b9648767281c34bb6ffd5fc0a2b8510a
Le Vivilet <levivlet@pm.me>
2024-12-25 14:46:45 +0100

    simplify code

--- packages/test-integration/src/a.js
+++ packages/test-integration/src/a.js
@@ -1,18 +1,23 @@
 import * as m from 'node:module'
 
-globalThis.mockModule = (moduleName, key, arg0, errorMessage, errorCode) => {
+globalThis.mockModule = (moduleName, options) => {
   const require = m.default.createRequire(import.meta.url)
   const mockedModule = require(moduleName)
-  const originalFn = mockedModule[key].bind(mockedModule)
+  const originalFn = mockedModule[options.functionName].bind(mockedModule)
   const mock = (...args) => {
-    if (args[0] === arg0) {
-      const error = new Error(errorMessage)
-      // @ts-ignore
-      error.code = errorCode
-      throw error
+    if (options.filePath && args[0] === options.filePath) {
+      if (options.errorMessage && options.errorCode) {
+        const error = new Error(options.errorMessage)
+        // @ts-ignore
+        error.code = options.errorCode
+        throw error
+      }
+      if (options.returnValue !== undefined) {
+        return options.returnValue
+      }
     }
     return originalFn(...args)
   }
-  mockedModule[key] = mock
+  mockedModule[options.functionName] = mock
   m.syncBuiltinESMExports()
 }
--- packages/test-integration/src/parts/MockModule/MockModule.ts
+++ packages/test-integration/src/parts/MockModule/MockModule.ts
@@ -10,47 +10,8 @@ interface MockOptions {
 }
 
 export const mockModule = async (runtime: CDP.Client['Runtime'], options: MockOptions): Promise<void> => {
-  const { moduleName, functionName, filePath, returnValue, errorMessage, errorCode } = options
-
-  let mockCode = `
-    const originalModule = await import('${moduleName}')
-    const mockedModule = { ...originalModule }
-    mockedModule['${functionName}'] = async (...args) => {
-  `
-
-  if (filePath) {
-    mockCode += `
-      const firstArg = args[0]
-      if (firstArg === '${filePath}') {
-    `
-  }
-
-  if (errorMessage && errorCode) {
-    mockCode += `
-        const error = new Error('${errorMessage}')
-        error.code = '${errorCode}'
-        throw error
-    `
-  } else if (returnValue) {
-    mockCode += `
-        return ${JSON.stringify(returnValue)}
-    `
-  }
-
-  if (filePath) {
-    mockCode += `
-      }
-      return originalModule['${functionName}'](...args)
-    `
-  }
-
-  mockCode += `
-    }
-    globalThis['${moduleName}'] = mockedModule
-  `
-
   await runtime.evaluate({
-    expression: mockCode,
+    expression: `mockModule('${options.moduleName}', ${JSON.stringify(options)})`,
     awaitPromise: true,
   })
 }
