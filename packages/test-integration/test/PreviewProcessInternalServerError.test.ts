0ec65436f3ff3d6f30819a4808dd357f05fb650b
Le Vivilet <levivlet@pm.me>
2024-12-25 15:03:51 +0100

    improve test

--- packages/test-integration/test/PreviewProcessInternalServerError.test.ts
+++ packages/test-integration/test/PreviewProcessInternalServerError.test.ts
@@ -13,7 +13,6 @@ test('preview process - internal server error', async () => {
   const root = getRoot()
   const rootPath = fileURLToPath(root)
   // TODO file path has duplicate slash for some reason, it should only have one slash or backslash.
-
   const slash = process.platform === 'win32' ? '\\' : '/'
   const filePath = `${rootPath}${slash}any-file.txt`
   const previewProcess = createPreviewProcess({
@@ -26,7 +25,12 @@ test('preview process - internal server error', async () => {
     moduleName: 'node:fs/promises',
     functionName: 'stat',
     filePath,
-    returnValue: { size: 1000 },
+    returnValue: {
+      size: 1000,
+      isDirectory: false,
+      isFile: true,
+      mtime: new Date('2024-01-01').toISOString(),
+    },
   })
 
   await mockModule(client.Runtime, {
