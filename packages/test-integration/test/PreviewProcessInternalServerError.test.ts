09aff3d87ebb922ae490ba012fa15e138300d8c1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 09:04:12 +0100

    Discard changes to packages/test-integration/test/PreviewProcessInternalServerError.test.ts

--- packages/test-integration/test/PreviewProcessInternalServerError.test.ts
+++ packages/test-integration/test/PreviewProcessInternalServerError.test.ts
@@ -1,50 +1,23 @@
 import { expect, test } from '@jest/globals'
 import getPort from 'get-port'
 import { fileURLToPath } from 'url'
-import { connectToCdp } from '../src/parts/ConnectToCdp/ConnectToCdp.ts'
 import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
 import { get } from '../src/parts/Get/Get.js'
 import { getRoot } from '../src/parts/GetRoot/GetRoot.js'
 
 test('preview process - internal server error', async () => {
   const debugPort = await getPort()
-  const ajs = new URL('../src/a.js', import.meta.url).toString()
+  const previewProcess = createPreviewProcess({
+    execArgv: [`--inspect=${debugPort}`, '--experimental-strip-types'],
+  })
+
   const root = getRoot()
   const rootPath = fileURLToPath(root)
   // TODO file path has duplicate slash for some reason, it should only have one slash or backslash.
   const slash = process.platform === 'win32' ? '\\' : '/'
   const filePath = `${rootPath}${slash}any-file.txt`
-  const previewProcess = createPreviewProcess({
-    execArgv: [`--inspect=${debugPort}`, '--experimental-vm-modules', '--experimental-strip-types', `--import=${ajs}`],
-  })
-  const client = await connectToCdp(debugPort)
-  const global = await client.Runtime.evaluate({
-    expression: 'globalThis',
-  })
-  await client.Runtime.callFunctionOn({
-    objectId: global.result.objectId,
-    functionDeclaration: `function(moduleName, key, arg0, errorMessage, errorCode){
-  const global = this
-  global.mockModule(moduleName, key, arg0, errorMessage, errorCode)
-}`,
-    arguments: [
-      {
-        value: 'node:fs/promises',
-      },
-      {
-        value: 'readFile',
-      },
-      {
-        value: filePath,
-      },
-      {
-        value: 'Access Denied',
-      },
-      {
-        value: 'EACCES',
-      },
-    ],
-  })
+
+  await previewProcess.invoke('Test.mockFs', 'node:fs/promises', 'readFile', filePath, 'Access Denied', 'EACCES')
 
   const id = 1
   const port = await getPort()
@@ -57,6 +30,5 @@ test('preview process - internal server error', async () => {
   expect(response.status).toBe(500)
   expect(await response.text()).toBe('Internal Server Error')
 
-  await client.close()
   previewProcess[Symbol.dispose]()
 })
