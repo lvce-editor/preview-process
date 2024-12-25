5ffa72429474b0ea981c80cfae17605f0af96f3c
Le Vivilet <levivlet@pm.me>
2024-12-25 22:35:53 +0100

    simplify code

--- packages/preview-process/src/parts/Routes/Routes.ts
+++ packages/preview-process/src/parts/Routes/Routes.ts
@@ -1,12 +1,16 @@
-import type { RouteHandler } from '../RouteHandler/RouteHandler.ts'
 import * as HandleIndexHtml from '../HandleIndexHtml/HandleIndexHtml.ts'
 import * as HandleOther from '../HandleOther/HandleOther.ts'
 import * as HandlePreviewInjected from '../HandlePreviewInjected/HandlePreviewInjected.ts'
+import * as ResolveFilePath from '../ResolveFilePath/ResolveFilePath.ts'
+import type { RouteHandler } from '../RouteHandler/RouteHandler.ts'
 
 export const routes: RouteHandler[] = [
   {
     pattern: /index\.html$/,
-    handler: HandleIndexHtml.handleIndexHtml,
+    handler: async (request, options): Promise<Response> => {
+      const filePath = ResolveFilePath.resolveFilePath(request.path, options.webViewRoot)
+      return HandleIndexHtml.handleIndexHtml(filePath, options)
+    },
   },
   {
     pattern: /preview-injected\.js$/,
@@ -14,6 +18,9 @@ export const routes: RouteHandler[] = [
   },
   {
     pattern: /.*/,
-    handler: HandleOther.handleOther,
+    handler: async (request, options): Promise<Response> => {
+      const filePath = ResolveFilePath.resolveFilePath(request.path, options.webViewRoot)
+      return HandleOther.handleOther(filePath, request, options)
+    },
   },
 ]
