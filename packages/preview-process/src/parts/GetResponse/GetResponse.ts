b847d6fd074e4db2adf64c3e1862d1ee6f863a29
Le Vivilet <levivlet@pm.me>
2024-12-25 14:09:06 +0100

    tsc

--- packages/preview-process/src/parts/GetResponse/GetResponse.ts
+++ packages/preview-process/src/parts/GetResponse/GetResponse.ts
@@ -5,12 +5,16 @@ import * as HttpMethod from '../HttpMethod/HttpMethod.ts'
 import { HeadResponse } from '../Responses/HeadResponse.ts'
 import { MethodNotAllowedResponse } from '../Responses/MethodNotAllowedResponse.ts'
 import * as Routes from '../Routes/Routes.ts'
+import { NotFoundResponse } from '../Responses/NotFoundResponse.ts'
 
 export const getResponse = async (request: RequestOptions, options: HandlerOptions): Promise<Response> => {
   if (request.method !== HttpMethod.Get && request.method !== HttpMethod.Head) {
     return new MethodNotAllowedResponse()
   }
   const matchedRoute = FindMatchingRoute.findMatchingRoute(request.path, Routes.routes)
+  if (!matchedRoute) {
+    return new NotFoundResponse()
+  }
   const response = await matchedRoute.handler(request, options)
   if (request.method === HttpMethod.Head) {
     return new HeadResponse(response.status, response.headers)
