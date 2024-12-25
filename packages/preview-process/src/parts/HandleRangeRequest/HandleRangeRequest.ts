a878c6c206b032bd93ff15db20afd92d1b05f33b
Le Vivilet <levivlet@pm.me>
2024-12-25 23:20:42 +0100

    feature: better handle open ended range requests

--- packages/preview-process/src/parts/HandleRangeRequest/HandleRangeRequest.ts
+++ packages/preview-process/src/parts/HandleRangeRequest/HandleRangeRequest.ts
@@ -7,26 +7,22 @@ import { RangeResponse } from '../Responses/RangeResponse.ts'
 export const handleRangeRequest = async (filePath: string, range: string): Promise<Response> => {
   const stats = await stat(filePath)
   const [x, y] = range.replace('bytes=', '').split('-')
-  const end = parseInt(y, 10)
   const start = parseInt(x, 10)
 
-  if (isNaN(start) || isNaN(end)) {
+  if (isNaN(start)) {
     return new BadRequestResponse('Invalid Range')
-  }
-
-  const options = {
-    start,
-    end: end || stats.size - 1,
-  }
-
-  if (end >= stats.size) {
-    options.end = stats.size - 1
   }
 
   if (start >= stats.size) {
     return new RangeNotSatisfiableResponse(stats.size)
   }
 
-  const readStream = createReadStream(filePath, options)
-  return new RangeResponse(readStream, start, options.end, stats.size)
+  const end = y ? parseInt(y, 10) : stats.size - 1
+  const finalEnd = end >= stats.size ? stats.size - 1 : end
+
+  const readStream = createReadStream(filePath, {
+    start,
+    end: finalEnd,
+  })
+  return new RangeResponse(readStream, start, finalEnd, stats.size)
 }
