fbfacb5ecc655f0ed49601171e4d633a9fb543e9
Le Vivilet <levivlet@pm.me>
2024-12-25 11:33:41 +0100

    fix response sending

--- packages/preview-process/src/parts/SendResponse/SendResponse.ts
+++ packages/preview-process/src/parts/SendResponse/SendResponse.ts
@@ -3,14 +3,14 @@ import { pipeline } from 'node:stream/promises'
 import * as IsStreamPrematureCloseError from '../IsStreamPrematureCloseError/IsStreamPrematureCloseError.ts'
 
 export const sendResponse = async (response: ServerResponse, result: Response): Promise<void> => {
-  if (!result?.body) {
-    response.end('not found')
-    return
-  }
   response.statusCode = result.status
   result.headers.forEach((value, key) => {
     response.setHeader(key, value)
   })
+  if (!result.body) {
+    response.end()
+    return
+  }
   try {
     await pipeline(result.body, response)
   } catch (error) {
