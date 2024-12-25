01e4be17111a3251d470c558cc255be2e604bc90
Le Vivilet <levivlet@pm.me>
2024-12-25 14:41:51 +0100

    feature: add options to mockmodule

--- packages/test-integration/src/parts/MockModule/MockModule.ts
+++ packages/test-integration/src/parts/MockModule/MockModule.ts
@@ -1,22 +1,56 @@
 import type CDP from 'chrome-remote-interface'
 
-export const mockModule = async (
-  runtime: CDP.Client['Runtime'],
-  moduleName: string,
-  key: string,
-  arg0: string,
-  errorMessage: string,
-  errorCode: string,
-): Promise<void> => {
-  const global = await runtime.evaluate({
-    expression: 'globalThis',
-  })
-  await runtime.callFunctionOn({
-    objectId: global.result.objectId,
-    functionDeclaration: `function(moduleName, key, arg0, errorMessage, errorCode){
-      const global = this
-      global.mockModule(moduleName, key, arg0, errorMessage, errorCode)
-    }`,
-    arguments: [{ value: moduleName }, { value: key }, { value: arg0 }, { value: errorMessage }, { value: errorCode }],
+interface MockOptions {
+  readonly moduleName: string
+  readonly functionName: string
+  readonly filePath?: string
+  readonly returnValue?: any
+  readonly errorMessage?: string
+  readonly errorCode?: string
+}
+
+export const mockModule = async (runtime: CDP.Client['Runtime'], options: MockOptions): Promise<void> => {
+  const { moduleName, functionName, filePath, returnValue, errorMessage, errorCode } = options
+
+  let mockCode = `
+    const originalModule = await import('${moduleName}')
+    const mockedModule = { ...originalModule }
+    mockedModule['${functionName}'] = async (...args) => {
+  `
+
+  if (filePath) {
+    mockCode += `
+      const firstArg = args[0]
+      if (firstArg === '${filePath}') {
+    `
+  }
+
+  if (errorMessage && errorCode) {
+    mockCode += `
+        const error = new Error('${errorMessage}')
+        error.code = '${errorCode}'
+        throw error
+    `
+  } else if (returnValue) {
+    mockCode += `
+        return ${JSON.stringify(returnValue)}
+    `
+  }
+
+  if (filePath) {
+    mockCode += `
+      }
+      return originalModule['${functionName}'](...args)
+    `
+  }
+
+  mockCode += `
+    }
+    globalThis['${moduleName}'] = mockedModule
+  `
+
+  await runtime.evaluate({
+    expression: mockCode,
+    awaitPromise: true,
   })
 }
