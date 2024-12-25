ede072d00e387c15549762a177dee619f17b3d2b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 13:53:38 +0100

    feature: use new range response classes (#198)

--- packages/preview-process/src/parts/HandleRangeRequest/HandleRangeRequest.ts
+++ packages/preview-process/src/parts/HandleRangeRequest/HandleRangeRequest.ts
@@ -1,23 +1,17 @@
 import { createReadStream } from 'node:fs'
 import { stat } from 'node:fs/promises'
-import * as HttpHeader from '../HttpHeader/HttpHeader.ts'
-import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.ts'
+import { BadRequestResponse } from '../Responses/BadRequestResponse.ts'
+import { RangeNotSatisfiableResponse } from '../Responses/RangeNotSatisfiableResponse.ts'
+import { RangeResponse } from '../Responses/RangeResponse.ts'
 
-// TODO add lots of tests for this
 export const handleRangeRequest = async (filePath: string, range: string): Promise<Response> => {
   const stats = await stat(filePath)
-  const code = HttpStatusCode.PartialContent
   const [x, y] = range.replace('bytes=', '').split('-')
   const end = parseInt(y, 10)
   const start = parseInt(x, 10)
 
   if (isNaN(start) || isNaN(end)) {
-    return new Response('Invalid Range', {
-      status: HttpStatusCode.BadRequest,
-      headers: {
-        [HttpHeader.ContentRange]: `bytes */${stats.size}`,
-      },
-    })
+    return new BadRequestResponse('Invalid Range')
   }
 
   const options = {
@@ -30,21 +24,9 @@ export const handleRangeRequest = async (filePath: string, range: string): Promi
   }
 
   if (start >= stats.size) {
-    return new Response(null, {
-      status: HttpStatusCode.RangeNotSatisfiable,
-      headers: {
-        [HttpHeader.ContentRange]: `bytes */${stats.size}`,
-      },
-    })
+    return new RangeNotSatisfiableResponse(stats.size)
   }
 
   const readStream = createReadStream(filePath, options)
-  return new Response(readStream, {
-    status: code,
-    headers: {
-      [HttpHeader.ContentRange]: `bytes ${start}-${options.end}/${stats.size}`,
-      [HttpHeader.ContentLength]: `${options.end - start + 1}`,
-      [HttpHeader.AcceptRanges]: 'bytes',
-    },
-  })
+  return new RangeResponse(readStream, start, options.end, stats.size)
 }
