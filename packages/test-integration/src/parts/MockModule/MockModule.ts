c5035e2a73ca0015b5323cba958463856934a945
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 22:56:57 +0100

    feature: improve error handling for mockModule error (#226)

--- packages/test-integration/src/parts/MockModule/MockModule.ts
+++ packages/test-integration/src/parts/MockModule/MockModule.ts
@@ -1,4 +1,5 @@
 import type CDP from 'chrome-remote-interface'
+import { VError } from '@lvce-editor/verror'
 
 interface MockOptions {
   readonly moduleName: string
@@ -10,8 +11,12 @@ interface MockOptions {
 }
 
 export const mockModule = async (runtime: CDP.Client['Runtime'], options: MockOptions): Promise<void> => {
-  await runtime.evaluate({
-    expression: `mockModule('${options.moduleName}', ${JSON.stringify(options)})`,
-    awaitPromise: true,
-  })
+  try {
+    await runtime.evaluate({
+      expression: `mockModule('${options.moduleName}', ${JSON.stringify(options)})`,
+      awaitPromise: true,
+    })
+  } catch (error) {
+    throw new VError(error, `Failed to mock module`)
+  }
 }
