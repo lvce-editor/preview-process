f95cc886b5f26d3bd82abc1a4fed64e4a5710fd0
Le Vivilet <levivlet@pm.me>
2024-12-25 14:11:59 +0100

    fix

--- packages/preview-process/src/parts/HandleOther/HandleOther.ts
+++ packages/preview-process/src/parts/HandleOther/HandleOther.ts
@@ -1,7 +1,6 @@
 import type { RequestOptions } from '../RequestOptions/RequestOptions.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetContentType from '../GetContentType/GetContentType.ts'
-import * as GetPathEtag from '../GetPathEtag/GetPathEtag.ts'
 import * as HandleRangeRequest from '../HandleRangeRequest/HandleRangeRequest.ts'
 import * as IsEnoentError from '../IsEnoentError/IsEnoentError.ts'
 import { ContentResponse } from '../Responses/ContentResponse.ts'
@@ -13,27 +12,6 @@ export const handleOther = async (filePath: string, requestOptions: RequestOptio
     if (requestOptions.range) {
       return await HandleRangeRequest.handleRangeRequest(filePath, requestOptions.range)
     }
-
-    const etag = await GetPathEtag.getPathEtag(filePath)
-    if (!etag) {
-      return new Response('not found', {
-        status: HttpStatusCode.NotFound,
-        headers: {
-          [HttpHeader.CrossOriginResourcePolicy]: 'same-origin',
-        },
-      })
-    }
-
-    if (request && MatchesEtag.matchesEtag(request, etag)) {
-      return new Response(null, {
-        status: HttpStatusCode.NotModified,
-        headers: {
-          [HttpHeader.CrossOriginResourcePolicy]: 'same-origin',
-          [HttpHeader.Etag]: etag,
-        },
-      })
-    }
-
     const contentType = GetContentType.getContentType(filePath)
     const content = await FileSystem.readFile(filePath)
     return new ContentResponse(content, contentType)
