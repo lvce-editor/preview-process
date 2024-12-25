e987a5bcf97dddfa55c294309975d1461591878e
Le Vivilet <levivlet@pm.me>
2024-12-25 09:29:43 +0100

    increase timeout

--- packages/test-integration/src/parts/ConnectToCdp/ConnectToCdp.ts
+++ packages/test-integration/src/parts/ConnectToCdp/ConnectToCdp.ts
@@ -2,29 +2,16 @@ import CDP from 'chrome-remote-interface'
 import waitPort from 'wait-port'
 
 export const connectToCdp = async (debugPort: number): Promise<CDP.Client> => {
-  try {
-    await waitPort({
-      port: debugPort,
-      host: '::1',
-      output: 'silent',
-    })
-    const client = await CDP({
-      host: '::1',
-      port: debugPort,
-    })
-    await client.Runtime.enable()
-    return client
-  } catch {
-    await waitPort({
-      port: debugPort,
-      host: '127.0.0.1',
-      output: 'silent',
-    })
-    const client = await CDP({
-      host: '127.0.0.1',
-      port: debugPort,
-    })
-    await client.Runtime.enable()
-    return client
-  }
+  await waitPort({
+    port: debugPort,
+    host: 'localhost',
+    output: 'silent',
+    timeout: 15000,
+  })
+  const client = await CDP({
+    host: 'localhost',
+    port: debugPort,
+  })
+  await client.Runtime.enable()
+  return client
 }
