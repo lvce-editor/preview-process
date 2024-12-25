e6c51a0dcc79505cca92f433b11260f8d8fccc4c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-25 12:06:04 +0100

    feature: add head request test (#177)

---
+++ packages/test-integration/test/PreviewProcessHeadRequest.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@jest/globals'
+import getPort from 'get-port'
+import { createPreviewProcess } from '../src/parts/CreatePreviewProcess/CreatePreviewProcess.js'
+import { getRoot } from '../src/parts/GetRoot/GetRoot.js'
+
+test.skip('preview process - handles HEAD requests', async () => {
+  const previewProcess = createPreviewProcess()
+  const id = 1
+  const port = await getPort()
+  const root = getRoot()
+
+  await previewProcess.invoke('WebViewServer.create', id)
+  await previewProcess.invoke('WebViewServer.setHandler', id, '', root, '', '')
+  await previewProcess.invoke('WebViewServer.start', id, port)
+
+  const response = await fetch(`http://localhost:${port}/package.json`, {
+    method: 'HEAD',
+  })
+
+  expect(response.status).toBe(200)
+  expect(response.headers.get('content-type')).toBe('application/json')
+  expect(await response.text()).toBe('')
+
+  previewProcess[Symbol.dispose]()
+})
