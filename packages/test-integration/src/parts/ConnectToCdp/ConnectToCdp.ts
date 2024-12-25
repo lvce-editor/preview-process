146d7ec5921ef035c224de1f3708ecb02f22ef5a
Le Vivilet <levivlet@pm.me>
2024-12-25 09:24:30 +0100

    try to fix windows

--- packages/test-integration/src/parts/ConnectToCdp/ConnectToCdp.ts
+++ packages/test-integration/src/parts/ConnectToCdp/ConnectToCdp.ts
@@ -2,15 +2,29 @@ import CDP from 'chrome-remote-interface'
 import waitPort from 'wait-port'
 
 export const connectToCdp = async (debugPort: number): Promise<CDP.Client> => {
-  await waitPort({
-    port: debugPort,
-    host: 'localhost',
-    output: 'silent',
-  })
-  const client = await CDP({
-    host: 'localhost',
-    port: debugPort,
-  })
-  await client.Runtime.enable()
-  return client
+  try {
+    await waitPort({
+      port: debugPort,
+      host: '::1',
+      output: 'silent',
+    })
+    const client = await CDP({
+      host: '::1',
+      port: debugPort,
+    })
+    await client.Runtime.enable()
+    return client
+  } catch {
+    await waitPort({
+      port: debugPort,
+      host: '127.0.0.1',
+      output: 'silent',
+    })
+    const client = await CDP({
+      host: '127.0.0.1',
+      port: debugPort,
+    })
+    await client.Runtime.enable()
+    return client
+  }
 }
