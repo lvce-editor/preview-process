e99e70cb52133a18f961457fadccb8e98f6b0485
Le Vivilet <levivlet@pm.me>
2024-12-25 22:34:39 +0100

    feature: update handleOther function

--- packages/preview-process/src/parts/HandleOther/HandleOther.ts
+++ packages/preview-process/src/parts/HandleOther/HandleOther.ts
@@ -1,22 +1,23 @@
 import { createReadStream } from 'node:fs'
-import type { HandlerOptions } from '../HandlerOptions/HandlerOptions.ts'
-import type { RequestOptions } from '../RequestOptions/RequestOptions.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetContentType from '../GetContentType/GetContentType.ts'
 import * as GetPathEtag from '../GetPathEtag/GetPathEtag.ts'
 import * as HandleRangeRequest from '../HandleRangeRequest/HandleRangeRequest.ts'
+import type { HandlerOptions } from '../HandlerOptions/HandlerOptions.ts'
 import * as IsEnoentError from '../IsEnoentError/IsEnoentError.ts'
 import * as MatchesEtag from '../MatchesEtag/MatchesEtag.ts'
-import * as ResolveFilePath from '../ResolveFilePath/ResolveFilePath.ts'
+import type { RequestOptions } from '../RequestOptions/RequestOptions.ts'
 import { ContentResponse } from '../Responses/ContentResponse.ts'
 import { NotFoundResponse } from '../Responses/NotFoundResponse.ts'
 import { NotModifiedResponse } from '../Responses/NotModifiedResponse.ts'
 import { ServerErrorResponse } from '../Responses/ServerErrorResponse.ts'
 
-export const handleOther = async (request: RequestOptions, handlerOptions: HandlerOptions): Promise<Response> => {
+export const handleOther = async (
+  filePath: string,
+  request: RequestOptions,
+  handlerOptions: HandlerOptions,
+): Promise<Response> => {
   try {
-    const filePath = ResolveFilePath.resolveFilePath(request.path, handlerOptions.webViewRoot)
-
     if (request.range) {
       return await HandleRangeRequest.handleRangeRequest(filePath, request.range)
     }
