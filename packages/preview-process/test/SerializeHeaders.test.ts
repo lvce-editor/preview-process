66786eb40ea521572dd73899afcdc8812f086b08
Le Vivilet <levivlet@pm.me>
2024-12-26 09:23:35 +0100

    feature: add header tests

---
+++ packages/preview-process/test/SerializeHeaders.test.ts
@@ -0,0 +1,44 @@
+import { expect, test } from '@jest/globals'
+import * as SerializeHeaders from '../src/parts/SerializeHeaders/SerializeHeaders.ts'
+
+test('serializeHeaders - empty headers', () => {
+  const headers = new Headers()
+  expect(SerializeHeaders.serializeHeaders(headers)).toEqual({})
+})
+
+test('serializeHeaders - single header', () => {
+  const headers = new Headers()
+  headers.set('Content-Type', 'text/plain')
+  expect(SerializeHeaders.serializeHeaders(headers)).toEqual({
+    'Content-Type': 'text/plain',
+  })
+})
+
+test('serializeHeaders - multiple headers', () => {
+  const headers = new Headers()
+  headers.set('Content-Type', 'text/plain')
+  headers.set('ETag', '"123"')
+  headers.set('Cross-Origin-Resource-Policy', 'same-origin')
+  expect(SerializeHeaders.serializeHeaders(headers)).toEqual({
+    'Content-Type': 'text/plain',
+    ETag: '"123"',
+    'Cross-Origin-Resource-Policy': 'same-origin',
+  })
+})
+
+test('serializeHeaders - case insensitive', () => {
+  const headers = new Headers()
+  headers.set('content-type', 'text/plain')
+  expect(SerializeHeaders.serializeHeaders(headers)).toEqual({
+    'Content-Type': 'text/plain',
+  })
+})
+
+test('serializeHeaders - multiple values for same header', () => {
+  const headers = new Headers()
+  headers.append('X-Custom', 'value1')
+  headers.append('X-Custom', 'value2')
+  expect(SerializeHeaders.serializeHeaders(headers)).toEqual({
+    'X-Custom': 'value1, value2',
+  })
+})
