eb5f0620829d359cf0e4f50e759d0716b7e69183
Le Vivilet <levivlet@pm.me>
2024-12-27 13:49:19 +0100

    feature: improve error handling when starting server fails

--- packages/preview-process/src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
+++ packages/preview-process/src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts
@@ -1,7 +1,32 @@
 import type { WebViewServer } from '../WebViewServerTypes/WebViewServerTypes.ts'
 
+interface FirstEvent {
+  readonly type: string
+  readonly event: any
+}
+
 export const waitForServerToBeReady = async (server: WebViewServer, port: string): Promise<void> => {
-  const { resolve, promise } = Promise.withResolvers<void>()
-  server.listen(port, resolve)
-  await promise
+  const { resolve, reject, promise } = Promise.withResolvers<FirstEvent>()
+  const handleError = (event: any) => {
+    cleanup({
+      type: 'error',
+      event,
+    })
+  }
+  const handleListening = (event: any): void => {
+    cleanup({
+      type: 'listening',
+      event,
+    })
+  }
+  const cleanup = (value: any): void => {
+    server.off('listening', handleListening)
+    server.off('error', handleError)
+    resolve(value)
+  }
+  server.listen(port, () => {})
+  const { type, event } = await promise
+  if (type === 'error') {
+    throw new Error(`server: ${event}`)
+  }
 }
--- packages/preview-process/test/WaitForServerToBeReady.test.ts
+++ packages/preview-process/test/WaitForServerToBeReady.test.ts
@@ -1,16 +1,39 @@
 import { expect, jest, test } from '@jest/globals'
 import * as WaitForServerToBeReady from '../src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts'
 
-test('waitForServerToBeReady', async () => {
+test('waitForServerToBeReady - success', async () => {
   const mockServer = {
+    server: {
+      once: jest.fn(),
+      listen: jest.fn((port, callback) => {
+        callback()
+      }),
+    },
     listen: jest.fn((port, callback) => {
-      // @ts-ignore
       callback()
     }),
   }
   const port = '3000'
-  // @ts-ignore
-  await WaitForServerToBeReady.waitForServerToBeReady(mockServer, port)
+  await WaitForServerToBeReady.waitForServerToBeReady(mockServer as any, port)
   expect(mockServer.listen).toHaveBeenCalledTimes(1)
   expect(mockServer.listen).toHaveBeenCalledWith(port, expect.any(Function))
+  expect(mockServer.server.once).toHaveBeenCalledWith('error', expect.any(Function))
+})
+
+test('waitForServerToBeReady - handles EADDRINUSE error', async () => {
+  const error = new Error('EADDRINUSE')
+  const mockServer = {
+    server: {
+      once: jest.fn((event, handler) => {
+        if (event === 'error') {
+          handler(error)
+        }
+      }),
+      listen: jest.fn(),
+    },
+    listen: jest.fn(),
+  }
+  const port = '3000'
+  await expect(WaitForServerToBeReady.waitForServerToBeReady(mockServer as any, port)).rejects.toThrow('EADDRINUSE')
+  expect(mockServer.server.once).toHaveBeenCalledWith('error', expect.any(Function))
 })
