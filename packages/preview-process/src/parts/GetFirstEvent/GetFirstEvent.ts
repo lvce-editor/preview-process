ee624a9a1728ac2b5bf48034b66965a07ca7b6d8
Le Vivilet <levivlet@pm.me>
2024-12-27 13:48:45 +0100

    feature: add getFirstEvent function

---
+++ packages/preview-process/src/parts/GetFirstEvent/GetFirstEvent.ts
@@ -0,0 +1,32 @@
+import type { EventEmitter } from 'node:events'
+import type { FirstEvent } from '../FirstEvent/FirstEvent.ts'
+
+const addListener = (emitter: EventEmitter, type: string, callback: any) => {
+  emitter.on(type, callback)
+}
+
+const removeListener = (emitter: EventEmitter, type: string, callback: any) => {
+  emitter.off(type, callback)
+}
+
+export const getFirstEvent = (eventEmitter: EventEmitter, eventMap: any): Promise<FirstEvent> => {
+  const { resolve, promise } = Promise.withResolvers<FirstEvent>()
+  const listenerMap = Object.create(null)
+  const cleanup = (value: any) => {
+    for (const event of Object.keys(eventMap)) {
+      removeListener(eventEmitter, event, listenerMap[event])
+    }
+    resolve(value)
+  }
+  for (const [event, type] of Object.entries(eventMap)) {
+    const listener = (event: any) => {
+      cleanup({
+        type,
+        event,
+      })
+    }
+    addListener(eventEmitter, event, listener)
+    listenerMap[event] = listener
+  }
+  return promise
+}
