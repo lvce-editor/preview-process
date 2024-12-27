958d5d2ec1640fe9ca599ee9525be6954ff9a4ce
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-27 13:51:19 +0100

    feature: improve server start error message (#256)

--- packages/preview-process/src/parts/WebViewServerState/WebViewServerState.ts
+++ packages/preview-process/src/parts/WebViewServerState/WebViewServerState.ts
@@ -1,13 +1,17 @@
 import type { WebViewServer } from '../WebViewServerTypes/WebViewServerTypes.ts'
 
-const servers = Object.create(null)
+const servers: Record<number, WebViewServer> = Object.create(null)
 
 export const set = (id: number, server: WebViewServer): void => {
   servers[id] = server
 }
 
 export const get = (id: number): WebViewServer => {
-  return servers[id]
+  const server = servers[id]
+  if (!server) {
+    throw new Error(`Server with id ${id} not found`)
+  }
+  return server
 }
 
 export const has = (id: number): boolean => {
