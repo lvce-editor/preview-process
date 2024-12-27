32168f9ca4f4f9a89ff6cf311131dd71bad99a32
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-27 16:13:29 +0100

    feature: support extra headers in notModifiedResponse (#280)

--- packages/preview-process/src/parts/Responses/NotModifiedResponse.ts
+++ packages/preview-process/src/parts/Responses/NotModifiedResponse.ts
@@ -2,12 +2,13 @@ import * as HttpHeader from '../HttpHeader/HttpHeader.ts'
 import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.ts'
 
 export class NotModifiedResponse extends Response {
-  constructor(etag: string) {
+  constructor(etag: string, extraHeaders: Record<string, string> = {}) {
     super(null, {
       status: HttpStatusCode.NotModified,
       headers: {
         [HttpHeader.CrossOriginResourcePolicy]: 'same-origin',
         [HttpHeader.Etag]: etag,
+        ...extraHeaders,
       },
     })
   }
