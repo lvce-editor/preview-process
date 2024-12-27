0bb7a8a04a6e6bf4fcc3970dc09a82f6557ad8f3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-27 15:34:57 +0100

    feature: remove unused code (#270)
    * feature: remove unused code
    
    * test

--- packages/preview-process/src/parts/GetPreviewInjectedResponse/GetPreviewInjectedResponse.ts
+++
@@ -1,7 +0,0 @@
-import * as GetHeaders from '../GetHeaders/GetHeaders.ts'
-import * as SuccessResponse from '../SuccessResponse/SuccessResponse.ts'
-
-export const getPreviewInjectedResponse = async (injectedCode: string): Promise<any> => {
-  const headers = GetHeaders.getHeaders('/test/file.js')
-  return SuccessResponse.create(injectedCode, headers)
-}
