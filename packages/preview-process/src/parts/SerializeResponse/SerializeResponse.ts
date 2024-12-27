0a008e18bfc080a656b0f4d1d86347cc9f8b2327
Le Vivilet <levivlet@pm.me>
2024-12-27 09:38:27 +0100

    remove tostring

--- packages/preview-process/src/parts/SerializeResponse/SerializeResponse.ts
+++ packages/preview-process/src/parts/SerializeResponse/SerializeResponse.ts
@@ -3,7 +3,7 @@ import * as SerializeHeaders from '../SerializeHeaders/SerializeHeaders.ts'
 export const serializeResponse = async (response: Response): Promise<any> => {
   const body = await response.arrayBuffer()
   return {
-    body: Buffer.from(body).toString(),
+    body: Buffer.from(body),
     init: {
       status: response.status,
       headers: SerializeHeaders.serializeHeaders(response.headers),
