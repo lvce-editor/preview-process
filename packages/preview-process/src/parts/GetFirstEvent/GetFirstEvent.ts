f60ed5afab696a285e8333b18da0f30677857892
Le Vivilet <levivlet@pm.me>
2024-12-27 13:52:24 +0100

    lint

--- packages/preview-process/src/parts/GetFirstEvent/GetFirstEvent.ts
+++ packages/preview-process/src/parts/GetFirstEvent/GetFirstEvent.ts
@@ -1,18 +1,18 @@
 import type { EventEmitter } from 'node:events'
 import type { FirstEvent } from '../FirstEvent/FirstEvent.ts'
 
-const addListener = (emitter: EventEmitter, type: string, callback: any) => {
+const addListener = (emitter: EventEmitter, type: string, callback: any): void => {
   emitter.on(type, callback)
 }
 
-const removeListener = (emitter: EventEmitter, type: string, callback: any) => {
+const removeListener = (emitter: EventEmitter, type: string, callback: any): void => {
   emitter.off(type, callback)
 }
 
 export const getFirstEvent = (eventEmitter: EventEmitter, eventMap: any): Promise<FirstEvent> => {
   const { resolve, promise } = Promise.withResolvers<FirstEvent>()
   const listenerMap = Object.create(null)
-  const cleanup = (value: any) => {
+  const cleanup = (value: any): void => {
     for (const event of Object.keys(eventMap)) {
       removeListener(eventEmitter, event, listenerMap[event])
     }
