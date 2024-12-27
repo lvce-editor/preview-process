1b0e6dcabdc60181c594a6bdd06ffd18dabb4a1a
Le Vivilet <levivlet@pm.me>
2024-12-27 15:07:23 +0100

    fix

--- packages/preview-process/src/parts/ResolveFilePath/ResolveFilePath.ts
+++ packages/preview-process/src/parts/ResolveFilePath/ResolveFilePath.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath } from 'node:url'
 
 export const resolveFilePath = (pathName: string, webViewRoot: string, remotePathPrefix: string = '/remote'): string => {
-  if (pathName.startsWith(remotePathPrefix)) {
-    const filePath = pathName.slice(remotePathPrefix.length)
+  // Handle remote paths
+  if (pathName.startsWith(`${remotePathPrefix}/`)) {
+    const filePath = pathName.slice(remotePathPrefix.length + 1) // +1 for the trailing slash
     return fileURLToPath(`file://${filePath}`)
   }
+  // Handle normal paths
   const filePath = fileURLToPath(`file://${webViewRoot}${pathName}`)
   return filePath
 }
