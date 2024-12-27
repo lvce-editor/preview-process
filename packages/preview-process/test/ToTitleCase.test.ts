f1c022fe0ac82beb28f66c056ced2e252ca30259
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-27 09:28:40 +0100

    feature: add toTitleCase function (#239)

---
+++ packages/preview-process/test/ToTitleCase.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@jest/globals'
+import * as ToTitleCase from '../src/parts/ToTitleCase/ToTitleCase.ts'
+
+test('toTitleCase - single word', () => {
+  expect(ToTitleCase.toTitleCase('content')).toBe('Content')
+})
+
+test('toTitleCase - multiple words', () => {
+  expect(ToTitleCase.toTitleCase('content-type')).toBe('Content-Type')
+})
+
+test('toTitleCase - already title case', () => {
+  expect(ToTitleCase.toTitleCase('Content-Type')).toBe('Content-Type')
+})
+
+test('toTitleCase - all uppercase', () => {
+  expect(ToTitleCase.toTitleCase('CONTENT-TYPE')).toBe('Content-Type')
+})
+
+test('toTitleCase - mixed case', () => {
+  expect(ToTitleCase.toTitleCase('ConTENT-tYPe')).toBe('Content-Type')
+})
+
+test('toTitleCase - multiple hyphens', () => {
+  expect(ToTitleCase.toTitleCase('cross-origin-resource-policy')).toBe('Cross-Origin-Resource-Policy')
+})
