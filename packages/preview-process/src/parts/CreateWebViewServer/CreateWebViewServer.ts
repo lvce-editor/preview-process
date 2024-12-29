303b07534e11ffc4db898dee95d0531f069ccb7a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 17:02:35 +0100

    feature: add option to server to use new request handler (#291)
    * feature: add option to server to use new request handler
    
    * lint

--- packages/preview-process/src/parts/CreateWebViewServer/CreateWebViewServer.ts
+++ packages/preview-process/src/parts/CreateWebViewServer/CreateWebViewServer.ts
@@ -1,14 +1,19 @@
 import { VError } from '@lvce-editor/verror'
 import { createServer } from 'node:http'
 import type { WebViewServer } from '../WebViewServerTypes/WebViewServerTypes.ts'
+import * as HandleRequest2 from '../HandleRequest2/HandleRequest2.ts'
 import * as WebViewServerState from '../WebViewServerState/WebViewServerState.ts'
 
-export const createWebViewServer = (id: number): void => {
+export const createWebViewServer = (id: number, useNewHandler?: boolean): void => {
   try {
     if (WebViewServerState.has(id)) {
       return
     }
     const server = createServer()
+    if (useNewHandler) {
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      server.on('request', HandleRequest2.handleRequest2)
+    }
     const webViewServer: WebViewServer = {
       handler: undefined,
       setHandler(handleRequest): void {
