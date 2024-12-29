586524dbf7a39cccc83782c5cf9886d07429fa17
Le Vivilet <levivlet@pm.me>
2024-12-29 15:35:05 +0100

    test

--- packages/preview-process/test/SetInfo2.test.ts
+++ packages/preview-process/test/SetInfo2.test.ts
@@ -1,12 +1,12 @@
 import { expect, test } from '@jest/globals'
 import * as GetInfo from '../src/parts/GetInfo/GetInfo.ts'
 import * as SetInfo2 from '../src/parts/SetInfo2/SetInfo2.ts'
+import { Info } from '../src/parts/Info.ts'
 
 test('setInfo2 - stores info correctly', () => {
-  const options: SetInfo2.SetInfoOptions = {
-    id: 1,
+  const options: Info = {
     webViewId: 'test-view',
-    webViewUri: '/test/root',
+    webViewRoot: 'file:///test/root',
     contentSecurityPolicy: "default-src 'self'",
     iframeContent: '<h1>Test Content</h1>',
   }
@@ -16,17 +16,16 @@ test('setInfo2 - stores info correctly', () => {
   const info = GetInfo.getInfo('lvce-webview://test-view')
   expect(info).toEqual({
     webViewId: options.webViewId,
-    webViewRoot: options.webViewUri,
+    webViewRoot: options.webViewRoot,
     contentSecurityPolicy: options.contentSecurityPolicy,
     iframeContent: options.iframeContent,
   })
 })
 
 test('setInfo2 - handles file:// URI correctly', () => {
-  const options: SetInfo2.SetInfoOptions = {
-    id: 1,
+  const options: Info = {
     webViewId: 'test-view',
-    webViewUri: 'file:///test/root',
+    webViewRoot: 'file:///test/root',
     contentSecurityPolicy: "default-src 'self'",
     iframeContent: '<h1>Test Content</h1>',
   }
@@ -36,7 +35,7 @@ test('setInfo2 - handles file:// URI correctly', () => {
   const info = GetInfo.getInfo('lvce-webview://test-view')
   expect(info).toEqual({
     webViewId: options.webViewId,
-    webViewRoot: '/test/root',
+    webViewRoot: 'file:///test/root',
     contentSecurityPolicy: options.contentSecurityPolicy,
     iframeContent: options.iframeContent,
   })
