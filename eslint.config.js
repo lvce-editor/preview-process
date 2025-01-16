d4cd79f514b1ebc6a576cede636716db18e7c707
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-16 22:05:25 +0100

    feature: use custom eslint config (#308)

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,10 @@
-import eslint from '@eslint/js'
-import pluginJest from 'eslint-plugin-jest'
-import nodePlugin from 'eslint-plugin-n'
-import perfectionist from 'eslint-plugin-perfectionist'
-import tseslint from 'typescript-eslint'
+import * as config from '@lvce-editor/eslint-config'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strictTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          type: 'natural',
-          order: 'asc',
-          newlinesBetween: 'never',
-        },
-      ],
-    },
-  },
-  nodePlugin.configs['flat/recommended'],
-  pluginJest.configs['flat/recommended'],
+export default [
+  ...config.default,
+  ...config.recommendedNode,
   {
     ignores: [
-      '.tmp',
-      'dist',
-      '**/coverage/**',
-      '**/preview-injected-code/**',
       'scripts',
       '**/mockCdpImport.js',
       'rollup.config.js',
@@ -50,34 +17,11 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/consistent-type-exports': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      'n/no-unpublished-import': [
-        'error',
-        {
-          allowModules: ['@jest/globals'],
-        },
-      ],
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/require-await': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/no-deprecated': 'off',
-      '@typescript-eslint/no-unnecessary-condition': 'off',
-      'jest/no-disabled-tests': 'off',
+      'n/no-unsupported-features/node-builtins': 'off',
       'n/no-unsupported-features/es-syntax': 'off',
-      '@typescript-eslint/no-base-to-string': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
     },
   },
-)
+]
