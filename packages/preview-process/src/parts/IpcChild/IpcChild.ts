b74254dc2540a4134ef13437ed1ee94b95923a6e
Le Vivilet <levivlet@pm.me>
2025-05-06 01:13:53 +0200

    fix

--- packages/preview-process/src/parts/IpcChild/IpcChild.ts
+++ packages/preview-process/src/parts/IpcChild/IpcChild.ts
@@ -4,7 +4,7 @@ import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 export const listen = async ({ method, ...params }: { method: number; [key: string]: any }): Promise<Rpc> => {
   const fn = IpcChildModule.getModule(method)
   // @ts-ignore
-  const rpc = await create(params)
+  const rpc = await fn(params)
   // @ts-ignore
   return rpc
 }
