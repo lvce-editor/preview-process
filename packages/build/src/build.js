db367a2ee1880e14f7f920ee9b527ac576d759bf
Le Vivilet <levivilet@pm.me>
2026-07-26 12:32:28 +0000

    feature: use npm workspaces

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -53,16 +53,7 @@ await mkdir(dist, { recursive: true })
 await bundleJs()
 
 await cp(
-  join(
-    root,
-    'packages',
-    'preview-process',
-    'node_modules',
-    '@lvce-editor',
-    'preview-injected-code',
-    'dist',
-    'previewInjectedCodeMain.js',
-  ),
+  join(root, 'node_modules', '@lvce-editor', 'preview-injected-code', 'dist', 'previewInjectedCodeMain.js'),
   join(root, '.tmp', 'dist', 'files', 'previewInjectedCode.js'),
 )
 
@@ -71,14 +62,8 @@ const version = await getVersion()
 const packageJson = await readJson(join(root, 'packages', 'preview-process', 'package.json'))
 
 const getWsVersion = async () => {
-  const { stdout } = await execa('npm', ['ls', 'ws', '--json'], {
-    cwd: join(root, 'packages', 'preview-process'),
-  })
-  const parsed = JSON.parse(stdout)
-  const wsVersion =
-    parsed.dependencies['@lvce-editor/rpc'].dependencies['@lvce-editor/ipc'].dependencies['@lvce-editor/web-socket-server']
-      .dependencies['ws'].version
-  return wsVersion
+  const packageJson = await readJson(join(root, 'node_modules', 'ws', 'package.json'))
+  return packageJson.version
 }
 
 const wsVersion = await getWsVersion()
@@ -114,7 +99,7 @@ await cp(join(root, 'LICENSE'), join(dist, 'LICENSE'))
 const indexJsPath = join(root, '.tmp', 'dist', 'dist', 'index.js')
 const oldContent = await readFile(indexJsPath, 'utf8')
 let newContent = oldContent.replace(
-  `const root = path.join(__dirname, '..', '..', '..');`,
+  `const root = path.join(__dirname, '..', '..', '..', '..', '..');`,
   `const root = path.join(__dirname, '..');`,
 )
 newContent = newContent.replace(
