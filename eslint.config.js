6f51934423dfb929ac2e248ec8f9e29cab42eb57
Le Vivilet <levivilet@pm.me>
2026-08-02 01:02:47 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedNode,
   {
@@ -40,4 +41,4 @@ export default [
       'unicorn/prefer-queue-microtask': 'off',
     },
   },
-]
+])
